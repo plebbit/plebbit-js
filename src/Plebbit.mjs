f741f4968a73ef148e728d2cfd7acf3379bb8555
Rinse <rinse12@tutanota.com>
2022-01-23 20:42:16 +0300

    Replaced raw http requests with ipfs-http-client

--- src/Plebbit.mjs
+++ src/Plebbit.mjs
@@ -1,13 +1,18 @@
-import fetch from 'node-fetch';
-import Subplebbit from "./Subplebbit.js";
-import Post from "./Post.js";
 import Comment from "./Comment.js";
+import Post from "./Post.js";
+import Subplebbit from "./Subplebbit.js";
+import {create as createIpfsClient} from 'ipfs-http-client';
+import {concat as uint8ArrayConcat} from 'uint8arrays/concat';
+import {toString as uint8ArrayToString} from 'uint8arrays/to-string';
+import all from 'it-all';
+import last from 'it-last';
 
 class Plebbit {
 
     constructor(options) {
         this.ipfsGatewayUrl = options["ipfsGatewayUrl"];
         this.ipfsApiUrl = options["ipfsApiUrl"];
+        this.ipfsClient = createIpfsClient(this.ipfsApiUrl);
     }
 
     setIpfsGatewayUrl(newGatewayUrl) {
@@ -16,38 +21,49 @@ class Plebbit {
 
     setIpfsApiUrl(newApiUrl) {
         this.ipfsApiUrl = newApiUrl;
+        this.ipfsClient = createIpfsClient(this.ipfsApiUrl);
     }
 
-    async getPost(postCid) {
-        // TODO add verification
+    async #loadIpfsFileAsJson(cid) {
         return new Promise((resolve, reject) => {
-            const url = `${this.ipfsApiUrl}/api/v0/cat?arg=${postCid}`;
-            fetch(url, {method: "POST"}).then(res => res.json())
-                .then(res => resolve(new Post(res)))
-                .catch(err => reject(err));
-        });
-    }
-
-    async getSubplebbit(subplebbitIpnsName) {
-        return new Promise((resolve, reject) => {
-            const resolveNameUrl = `${this.ipfsApiUrl}/api/v0/name/resolve?arg=${subplebbitIpnsName}`;
-            fetch(resolveNameUrl, {method: "POST"}).then(res => res.json())
-                .then(pathRes => {
-                    const ipfsPath = pathRes["Path"].split("/ipfs/")[1];
-                    const ipfsSubplebbitUrl = `${this.ipfsApiUrl}/api/v0/cat?arg=${ipfsPath}`;
-                    fetch(ipfsSubplebbitUrl, {method: "POST"}).then(res => res.json())
-                        .then(res => resolve(new Subplebbit(res)))
-                        .catch(err => reject(err));
+            all(this.ipfsClient.cat(cid))
+                .then(rawData => uint8ArrayConcat(rawData))
+                .catch(reject)
+                .then(data => {
+                    const jsonObject = JSON.parse(uint8ArrayToString(data));
+                    resolve(jsonObject);
                 }).catch(reject);
         });
     }
 
-    async getComment(commentCid) {
+    async getPost(postCid) {
+        // TODO add verification
+        const plebbit = this;
         return new Promise((resolve, reject) => {
-            const url = `${this.ipfsApiUrl}/api/v0/cat?arg=${commentCid}`;
-            fetch(url, {method: "POST"}).then(res => res.json())
-                .then(res => resolve(new Comment(res)))
-                .catch(err => reject(err));
+            plebbit.#loadIpfsFileAsJson(postCid)
+                .then(jsonFile => resolve(new Post(jsonFile)))
+                .catch(reject);
+        });
+    }
+
+    async getSubplebbit(subplebbitIpnsName) {
+        if (!subplebbitIpnsName.includes("/ipns/"))
+            subplebbitIpnsName = `/ipns/${subplebbitIpnsName}`;
+        const plebbit = this;
+        return new Promise(async (resolve, reject) => {
+            const subplebbitCid = await last(this.ipfsClient.name.resolve(subplebbitIpnsName));
+            plebbit.#loadIpfsFileAsJson(subplebbitCid)
+                .then(jsonFile => resolve(new Subplebbit(jsonFile)))
+                .catch(reject);
+        });
+    }
+
+    async getComment(commentCid) {
+        const plebbit = this;
+        return new Promise((resolve, reject) => {
+            plebbit.#loadIpfsFileAsJson(commentCid)
+                .then(jsonFile => resolve(new Comment(jsonFile)))
+                .catch(reject);
         });
     };
 }
