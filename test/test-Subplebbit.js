26db3b58b5ebe2922cdd47388d7350495786b054
Rinse <rinse12@tutanota.com>
2022-01-24 19:36:01 +0300

    Added unit tests publishAsNewSubplebbit

---
+++ test/test-Subplebbit.js
@@ -0,0 +1,26 @@
+import {IPFS_API_URL, IPFS_GATEWAY_URL} from "../secrets.js";
+import assert from 'assert';
+import Subplebbit from "../src/Subplebbit.js";
+import Plebbit from "../src/Plebbit.mjs";
+
+const plebbit = new Plebbit({ipfsGatewayUrl: IPFS_GATEWAY_URL, ipfsApiUrl: IPFS_API_URL})
+const subplebbit = new Subplebbit({
+    "title": `Test subplebbit - ${new Date().getMilliseconds()}`,
+    "ipfsGatewayUrl": IPFS_GATEWAY_URL,
+    "ipfsApiUrl": IPFS_API_URL
+});
+
+describe("Test Subplebbit", async () => {
+
+    // Destroy subplebbit once we're done with tests
+    after(async () => await subplebbit.destroy());
+
+
+
+    it("Can publish new subplebbit to ipfs", async function () {
+        await subplebbit.publishAsNewSubplebbit();
+        // Should have ipns key now
+        const loadedSubplebbit = await plebbit.getSubplebbit(subplebbit.ipnsKeyId);
+        assert.equal(JSON.stringify(loadedSubplebbit), JSON.stringify(subplebbit), "Failed to publish new subplebbit");
+    });
+});
