889e091db0ae64f74a346722dd0885705638e0db
Rinse <rinse12@tutanota.com>
2022-01-24 22:13:49 +0300

    - instead of holding an ipfs instance per Plebbit/Subplebbit, now it's held in only Plebbit, and to instantiate Subplebbit you need to provide a Plebbit instance

--- test/test-Subplebbit.js
+++ test/test-Subplebbit.js
@@ -6,16 +6,12 @@ import Post from "../src/Post.js";
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 const plebbit = new Plebbit({ipfsGatewayUrl: IPFS_GATEWAY_URL, ipfsApiUrl: IPFS_API_URL})
 const subplebbit = new Subplebbit({
-    "title": `Test subplebbit - ${new Date().getMilliseconds()}`,
-    "ipfsGatewayUrl": IPFS_GATEWAY_URL,
-    "ipfsApiUrl": IPFS_API_URL
-});
+    "title": `Test subplebbit - ${new Date().getMilliseconds()}`}, plebbit);
 
 describe("Test Subplebbit", async () => {
 
     // Destroy subplebbit once we're done with tests
     after(async () => await subplebbit.destroy());
-
 
 
     it("Can publish new subplebbit to ipfs", async function () {
