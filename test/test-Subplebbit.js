dca4fe630e38897189e452d9eb5d8ab85871734e
Rinse <rinse12@tutanota.com>
2022-01-25 15:15:09 +0300

    Added unit test for publishing new posts

--- test/test-Subplebbit.js
+++ test/test-Subplebbit.js
@@ -3,11 +3,11 @@ import assert from 'assert';
 import Subplebbit from "../src/Subplebbit.js";
 import Plebbit from "../src/Plebbit.js";
 import Post from "../src/Post.js";
-import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
-const plebbit = new Plebbit({ipfsGatewayUrl: IPFS_GATEWAY_URL, ipfsApiUrl: IPFS_API_URL})
-const subplebbit = new Subplebbit({
-    "title": `Test subplebbit - ${new Date().getMilliseconds()}`}, plebbit);
 
+const plebbit = new Plebbit({ipfsGatewayUrl: IPFS_GATEWAY_URL, ipfsApiUrl: IPFS_API_URL});
+const subplebbit = new Subplebbit({
+    "title": `Test subplebbit - ${new Date().getMilliseconds()}`
+}, plebbit);
 describe("Test Subplebbit", async () => {
 
     // Destroy subplebbit once we're done with tests
@@ -19,5 +19,27 @@ describe("Test Subplebbit", async () => {
         // Should have ipns key now
         const loadedSubplebbit = await plebbit.getSubplebbit(subplebbit.ipnsKeyId);
         assert.equal(JSON.stringify(loadedSubplebbit), JSON.stringify(subplebbit), "Failed to publish new subplebbit");
+    });
+
+    it("Can publish new posts", async function () {
+
+        // Ready subplebbit for post
+        await subplebbit.startPublishing();
+
+        const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${Date.now()}`);
+        const mockPost = new Post({
+            "author": {"displayName": "Mock Author", "ipnsId": mockAuthorIpns["id"]},
+            "title": "Mock Post", "content": "Mock content", "timestamp": Date.now(),
+        }, plebbit, subplebbit);
+
+        await mockPost.publishPost();
+
+        return new Promise((resolve, reject) => {
+            subplebbit.on('post', (post) => {
+                assert.equal(post.title, mockPost.title, "Failed to publish correct post");
+                assert.equal(post.cid, subplebbit.latestPostCid, "Failed to update subplebbit latestPostCid");
+                resolve();
+            });
+        });
     });
 });
