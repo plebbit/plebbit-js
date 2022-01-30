121feef254c599be47ecf62487f8a234c3ae340f
Rinse <rinse12@tutanota.com>
2022-01-30 19:10:27 +0300

    Added a unit test for publishing comments under comments

--- test/test-Post.js
+++ test/test-Post.js
@@ -5,7 +5,9 @@ import assert from 'assert';
 
 const plebbit = new Plebbit({ipfsGatewayUrl: IPFS_GATEWAY_URL, ipfsApiUrl: IPFS_API_URL});
 
-const post = await plebbit.getPostOrComment("QmP3N6nzXd8Z9ijHDHPxMemmzk2vMZvGvWgZfTy7Fmco6U");
+const post = await plebbit.getPostOrComment("QmaUA9jUp3CH96usYNwYXe58HnaxjeAHKKhthk55XNTU2d");
+
+const mockComments = [];
 
 async function generateMockComment(parentPostOrComment) {
     const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${Date.now()}`);
@@ -35,6 +37,7 @@ describe("Test Post and Comment", async function () {
                 const loadedParentPost = await plebbit.getPostOrComment(comment.postCid);
                 await loadedParentPost.fetchCommentIpns();
                 assert.equal(loadedParentPost.commentIpns.latestCommentCid, comment.commentCid.toString(), "Failed to include latest comment in Post");
+                mockComments.push(comment);
                 resolve();
             });
             await mockComment.subplebbit.startPublishing();
@@ -43,6 +46,24 @@ describe("Test Post and Comment", async function () {
         });
 
 
+    });
+    it("Can publish new comments under comments", async () => {
+        return new Promise(async (resolve, reject) => {
+            const mockComment = await generateMockComment(mockComments[0]);
+            mockComment.subplebbit.once("comment", async (comment) => {
+                await comment.fetchParent();
+                assert.equal(comment.parent.commentCid, mockComments[0].commentCid.toString());
+                assert.equal(comment.parentCommentCid, mockComments[0].commentCid.toString());
+
+                await comment.parent.fetchCommentIpns();
+                assert.equal(comment.parent.commentIpns.latestCommentCid, comment.commentCid.toString());
+                assert.equal(comment.parent.commentIpns.preloadedComments[0].commentCid, comment.commentCid.toString());
+                mockComments.push(comment);
+                resolve();
+            });
+            await mockComment.subplebbit.startPublishing();
+            await mockComment.publish();
+        });
     });
 
 });
