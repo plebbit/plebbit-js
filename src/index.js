c274c754d8884059ab5cded8d22d9283fb3aac1d
Rinse <rinse12@tutanota.com>
2022-02-10 21:50:25 +0300

    Changed package name,  version (temp, will change later), and main entry file to index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Plebbit from "./Plebbit.js";
+import Subplebbit from "./Subplebbit.js";
+import Comment from "./Comment.js";
+import Post from "./Post.js";
+import Vote from "./Vote.js";
+
+export default Plebbit;
+export {Plebbit, Subplebbit, Comment, Post, Vote};
