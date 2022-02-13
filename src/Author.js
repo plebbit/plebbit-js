f9fe4b9b5dc8574e787b6d2e549f31ef4a41388d
Rinse <rinse12@tutanota.com>
2022-02-14 00:37:40 +0300

    Changed naming from ipnsKeyId to ipnsName

--- src/Author.js
+++ src/Author.js
@@ -1,7 +1,11 @@
 class Author {
     constructor(props) {
         this.displayName = props["displayName"];
-        this.ipnsKeyId = props["ipnsKeyId"];
+        this.ipnsName = props["ipnsName"];
+    }
+
+    toJSON() {
+        return {"ipnsName": this.ipnsName, "displayName": this.displayName};
     }
 }
 
