2cbe65bf9631e43f84237dd28e1922ede80bf7be
Rinse <rinse12@tutanota.com>
2022-02-15 13:47:17 +0300

    Rewrote test-Vote:
    - No separate test cases for Comment and Post since they're essentially the same thing. Added extra test cases for testing whether an exception is risen if a vote is published twice. Another test cases include changing upvote to downvote, canceling a downvote or upvote

--- test/test-Vote.js
+++ test/test-Vote.js
@@ -7,6 +7,7 @@ const plebbit = new Plebbit({ipfsGatewayUrl: IPFS_GATEWAY_URL, ipfsApiUrl: IPFS_
 
 const post = await plebbit.getPostOrComment("QmeqbvRcRv7L5jvb9hLmD3LGcnjuLAcpLbngsiZRrisT3L");
 const comment = await plebbit.getPostOrComment("QmX3tgDHFYEaYZnEb6P2Qci9E6GfDWGTBhDAmZ1qLdxUCk");
+const previousVotes = [];
 const generateMockVote = async (parentPostOrComment, vote) => {
     const voteTime = Date.now();
     const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${voteTime}`);
@@ -20,7 +21,7 @@ const generateMockVote = async (parentPostOrComment, vote) => {
 describe("Test Vote", async () => {
     before(async () => await unsubscribeAllPubsubTopics(plebbit.ipfsClient));
 
-    it("Can upvote a post", async () => {
+    it("Can upvote a comment", async () => {
         return new Promise(async (resolve, reject) => {
             await post.fetchCommentIpns();
 
@@ -33,38 +34,55 @@ describe("Test Vote", async () => {
             vote.publish().then(async (challengeWithVote) => {
                 await post.fetchCommentIpns();
                 assert.equal(post.commentIpns.upvoteCount, originalUpvote + 1);
+                previousVotes.push(vote);
                 resolve();
             }).catch(reject);
         });
 
     });
 
-    it("Can downvote a post", async () => {
+    it("Throws an error when vote is duplicated", async () => {
+        return new Promise(async (resolve, reject) => {
+            const vote = new Vote({...previousVotes[0].toJSON(), "timestamp": Date.now()}, previousVotes[0].subplebbit);
+            vote.publish().then(reject).catch(resolve);
+        });
+
+    });
+
+    it("Can change upvote to downvote", async () => {
         return new Promise(async (resolve, reject) => {
             await post.fetchCommentIpns();
+
+            const originalUpvote = post.commentIpns.upvoteCount;
             const originalDownvote = post.commentIpns.downvoteCount;
-            const vote = await generateMockVote(post, -1);
+            const vote = new Vote({
+                ...previousVotes[0].toJSON(),
+                "vote": -1,
+                "timestamp": Date.now()
+            }, previousVotes[0].subplebbit);
             vote.publish().then(async (challengeWithVote) => {
                 await post.fetchCommentIpns();
-                assert.equal(post.commentIpns.downvoteCount, originalDownvote + 1);
+                assert.equal(post.commentIpns.upvoteCount, originalUpvote - 1, "Failed to update upvote count");
+                assert.equal(post.commentIpns.downvoteCount, originalDownvote + 1, "Failed to update downvote count");
                 resolve();
             }).catch(reject);
         });
 
     });
 
-    it("Can upvote a comment", async () => {
+    it("Can cancel a comment downvote", async () => {
         return new Promise(async (resolve, reject) => {
-            await comment.fetchCommentIpns();
-            const originalUpvote = comment.commentIpns.upvoteCount;
-            const vote = await generateMockVote(comment, 1);
-            vote.subplebbit.setProvideCaptchaCallback((challengeWithMsg) => {
-                return [null, null, "Captcha is skipped for all"];
-            });
-            await vote.subplebbit.startPublishing();
+            await post.fetchCommentIpns();
+
+            const originalDownvote = post.commentIpns.downvoteCount;
+            const vote = new Vote({
+                ...previousVotes[0].toJSON(),
+                "vote": 0,
+                "timestamp": Date.now()
+            }, previousVotes[0].subplebbit);
             vote.publish().then(async (challengeWithVote) => {
-                await comment.fetchCommentIpns();
-                assert.equal(comment.commentIpns.upvoteCount, originalUpvote + 1);
+                await post.fetchCommentIpns();
+                assert.equal(post.commentIpns.downvoteCount, originalDownvote - 1);
                 resolve();
             }).catch(reject);
         });
@@ -73,12 +91,53 @@ describe("Test Vote", async () => {
 
     it("Can downvote a comment", async () => {
         return new Promise(async (resolve, reject) => {
-            await comment.fetchCommentIpns();
-            const originalDownvote = comment.commentIpns.downvoteCount;
-            const vote = await generateMockVote(comment, -1);
+            await post.fetchCommentIpns();
+            const originalDownvote = post.commentIpns.downvoteCount;
+            const vote = await generateMockVote(post, -1);
             vote.publish().then(async (challengeWithVote) => {
-                await comment.fetchCommentIpns();
-                assert.equal(comment.commentIpns.downvoteCount, originalDownvote + 1);
+                await post.fetchCommentIpns();
+                assert.equal(post.commentIpns.downvoteCount, originalDownvote + 1);
+                previousVotes.push(vote);
+                resolve();
+            }).catch(reject);
+        });
+
+    });
+
+    it("Can change downvote to upvote", async () => {
+        return new Promise(async (resolve, reject) => {
+            await post.fetchCommentIpns();
+
+            const originalUpvote = post.commentIpns.upvoteCount;
+            const originalDownvote = post.commentIpns.downvoteCount;
+            const vote = new Vote({
+                ...previousVotes[1].toJSON(),
+                "vote": 1,
+                "timestamp": Date.now()
+            }, previousVotes[1].subplebbit);
+            vote.publish().then(async (challengeWithVote) => {
+                await post.fetchCommentIpns();
+                assert.equal(post.commentIpns.upvoteCount, originalUpvote + 1, "Failed to update upvote count");
+                assert.equal(post.commentIpns.downvoteCount, originalDownvote - 1, "Failed to update downvote count");
+                resolve();
+            }).catch(reject);
+        });
+
+    });
+
+    it("Can cancel a comment upvote", async () => {
+        return new Promise(async (resolve, reject) => {
+            await post.fetchCommentIpns();
+
+            const originalUpvote = post.commentIpns.upvoteCount;
+            const vote = new Vote({
+                ...previousVotes[1].toJSON(),
+                "vote": 0,
+                "timestamp": Date.now()
+            }, previousVotes[1].subplebbit);
+            vote.publish().then(async (challengeWithVote) => {
+                await post.fetchCommentIpns();
+                assert.equal(post.commentIpns.upvoteCount, originalUpvote - 1);
                 resolve();
             }).catch(reject);
         });
