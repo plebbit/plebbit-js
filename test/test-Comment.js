f074f8baec84db74913d8e3ebaba1bfaac9257f0
Rinse <rinse12@tutanota.com>
2022-03-20 19:27:21 +0000

    Update test-comment.js to use latest code

--- test/test-Comment.js
+++ test/test-Comment.js
@@ -23,17 +23,19 @@ describe("Test Post and Comment", async function () {
     it("Can publish new comment under post", async function () {
         return new Promise(async (resolve, reject) => {
             const mockComment = await generateMockComment(post, post.subplebbit);
+            await post.update();
+            const originalReplyCount = post.replyCount;
             mockComment.publish().then(async (challengeVerificationMessage) => {
                 const loadedComment = await plebbit.getPostOrComment(mockComment.commentCid);
+                await loadedComment.update();
                 assert.equal(JSON.stringify(loadedComment), JSON.stringify(mockComment));
 
                 await mockComment.fetchParent();
-                await mockComment.parent.fetchCommentIpns();
-                assert.equal(mockComment.parent.commentIpns.latestCommentCid, mockComment.commentCid);
-                assert.equal(mockComment.parent.commentIpns.preloadedComments[0].commentCid, mockComment.commentCid);
-                const loadedParentPost = await plebbit.getPostOrComment(mockComment.postCid);
-                await loadedParentPost.fetchCommentIpns();
-                assert.equal(loadedParentPost.commentIpns.latestCommentCid, mockComment.commentCid, "Failed to include latest comment in Post");
+                await mockComment.parent.update();
+                const latestCommentCid = (await loadIpfsFileAsJson(mockComment.parent.sortedRepliesCids[SORTED_COMMENTS_TYPES.NEW], plebbit.ipfsClient)).comments[0].commentCid;
+                assert.equal(latestCommentCid, mockComment.commentCid);
+                await post.update();
+                assert.equal(post.replyCount, originalReplyCount + 1, "Failed to update reply count");
                 mockComments.push(mockComment);
                 resolve();
             }).catch(reject);
@@ -47,10 +49,13 @@ describe("Test Post and Comment", async function () {
             actualComments.push(await generateMockComment(post, post.subplebbit));
             await sleep(1000);
         }
+        await post.update();
+        const originalReplyCount = post.replyCount;
         await Promise.all(actualComments.map(async post => post.publish()));
         mockComments.push(actualComments[0]);
-        const commentIpns = await post.fetchCommentIpns();
-        let sortedCommentsPage = new SortedComments(await loadIpfsFileAsJson(commentIpns.sortedCommentsCids[SORTED_COMMENTS_TYPES.NEW], plebbit.ipfsClient));
+        await sleep(10000);
+        await post.update();
+        let sortedCommentsPage = new SortedComments(await loadIpfsFileAsJson(post.sortedRepliesCids[SORTED_COMMENTS_TYPES.NEW], plebbit.ipfsClient));
         let sortedComments = sortedCommentsPage.comments;
         while (sortedCommentsPage.nextSortedCommentsCid) {
             sortedCommentsPage = new SortedComments(await loadIpfsFileAsJson(sortedCommentsPage.nextSortedCommentsCid, plebbit.ipfsClient));
@@ -59,21 +64,24 @@ describe("Test Post and Comment", async function () {
         for (let i = 0; i < sortedComments.length - 1; i++)
             if (sortedComments[i].timestamp < sortedComments[i + 1].timestamp)
                 assert.fail("New Comments under a post are not sorted according to their timestamp");
-
+        await post.update();
+        assert.equal(post.replyCount, originalReplyCount + actualComments.length, "Reply count failed to update");
     });
 
 
     it("Can publish new comments under comment", async () => {
         return new Promise(async (resolve, reject) => {
             const mockComment = await generateMockComment(mockComments[0], mockComments[0].subplebbit);
+            await mockComments[0].update();
+            const originalReplyCount = mockComments[0].replyCount;
             mockComment.publish().then(async challengeVerificationMessage => {
                 await mockComment.fetchParent();
                 assert.equal(mockComment.parent.commentCid, mockComments[0].commentCid);
                 assert.equal(mockComment.parentCommentCid, mockComments[0].commentCid);
-
-                await mockComment.parent.fetchCommentIpns();
-                assert.equal(mockComment.parent.commentIpns.latestCommentCid, mockComment.commentCid);
-                assert.equal(mockComment.parent.commentIpns.preloadedComments[0].commentCid, mockComment.commentCid);
+                await mockComment.parent.update();
+                const latestCommentCid = (await loadIpfsFileAsJson(mockComment.parent.sortedRepliesCids[SORTED_COMMENTS_TYPES.NEW], plebbit.ipfsClient)).comments[0].commentCid;
+                assert.equal(latestCommentCid, mockComment.commentCid);
+                assert.equal(mockComment.parent.replyCount, originalReplyCount + 1);
                 mockComments.push(mockComment);
                 resolve();
             }).catch(reject);
