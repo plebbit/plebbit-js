b79c305f83112dc6127d0b9fe9470225dcb430e1
Rinse <rinse12@tutanota.com>
2022-04-07 20:19:09 +0000

    Respond to challenge with "undefined" by default for tests

--- test/MockUtil.js
+++ test/MockUtil.js
@@ -4,50 +4,67 @@ import {SortedComments} from "../src/SortHandler.js";
 export async function generateMockComment(parentPostOrComment, subplebbit) {
     const commentTime = Date.now() / 1000;
     const mockAuthorIpns = await subplebbit.plebbit.ipfsClient.key.gen(`Mock User - ${commentTime}`);
-    return subplebbit.plebbit.createComment({
+    const comment = await subplebbit.plebbit.createComment({
         "author": {"displayName": `Mock Author - ${commentTime}`, "address": mockAuthorIpns["id"]},
         "content": `Mock comment - ${commentTime}`,
         "postCid": parentPostOrComment.postCid,
         "parentCid": parentPostOrComment.commentCid,
         "subplebbitAddress": subplebbit.subplebbitAddress
     });
+    comment.once("challenge", challengeMsg => {
+        comment.publishChallengeAnswers(undefined);
+    });
+    return comment;
 }
 
-export async function generateMockPost(subplebbit) {
+export async function generateMockPost(subplebbitAddress, plebbit) {
     const postStartTestTime = Date.now() / 1000;
-    const mockAuthorIpns = await subplebbit.plebbit.ipfsClient.key.gen(`Mock User - ${postStartTestTime}`);
-    return subplebbit.plebbit.createComment({
+    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${postStartTestTime}`);
+    const post = await plebbit.createComment({
         "author": {"displayName": `Mock Author - ${postStartTestTime}`, "address": mockAuthorIpns["id"]},
         "title": `Mock Post - ${postStartTestTime}`,
         "content": `Mock content - ${postStartTestTime}`,
-        "subplebbitAddress": subplebbit.subplebbitAddress
+        "subplebbitAddress": subplebbitAddress
     });
+    post.once("challenge", challengeMsg => {
+        post.publishChallengeAnswers(undefined);
+    });
+    return post
 }
 
-export async function generateMockPostWithRandomTimestamp(subplebbit) {
+export async function generateMockPostWithRandomTimestamp(subplebbitAddress, plebbit) {
     const randomTimeframeIndex = Math.floor(Math.random() * (Object.keys(TIMEFRAMES_TO_SECONDS).length - 1));
     const postTimestamp = timestamp() - (Math.random > 0.5 ? TIMEFRAMES_TO_SECONDS[randomTimeframeIndex] : 0);
     const postTime = Date.now();
-    const mockAuthorIpns = await subplebbit.plebbit.ipfsClient.key.gen(`Mock User - ${postTime}`);
-    return subplebbit.plebbit.createComment({
+    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${postTime}`);
+    const post = await plebbit.createComment({
         "author": {"displayName": `Mock Author - ${postTime}`, "address": mockAuthorIpns["id"]},
         "title": `Mock Post - ${postTime}`,
         "content": `Mock content - ${postTime}`,
-        "subplebbitAddress": subplebbit.subplebbitAddress,
+        "subplebbitAddress": subplebbitAddress,
         "timestamp": postTimestamp
     });
 
+    post.once("challenge", challengeMsg => {
+        post.publishChallengeAnswers(undefined);
+    });
+    return post;
+
 }
 
-export async function generateMockVote(parentPostOrComment, vote, subplebbit) {
+export async function generateMockVote(parentPostOrComment, vote, subplebbitAddress, plebbit) {
     const voteTime = Date.now() / 1000;
-    const mockAuthorIpns = await subplebbit.plebbit.ipfsClient.key.gen(`Mock User - ${voteTime}`);
-    return subplebbit.plebbit.createVote({
+    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${voteTime}`);
+    const voteObj = await plebbit.createVote({
         "author": {"displayName": `Mock Author - ${voteTime}`, "address": mockAuthorIpns["id"]},
         "commentCid": parentPostOrComment.commentCid || parentPostOrComment.postCid,
         "vote": vote,
-        "subplebbitAddress": subplebbit.subplebbitAddress
+        "subplebbitAddress": subplebbitAddress
     });
+    voteObj.once("challenge", challengeMsg => {
+        voteObj.publishChallengeAnswers(undefined);
+    });
+    return voteObj;
 }
 
 export async function loadAllPagesThroughSortedComments(sortedCommentsCid, plebbit) {
