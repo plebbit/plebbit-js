c3a59040fd4a37b26c982abe20ec98ae8d8dc871
Rinse <rinse12@tutanota.com>
2022-04-14 11:02:32 +0000

    Using signer in generating mock comments/votes

--- test/MockUtil.js
+++ test/MockUtil.js
@@ -2,9 +2,10 @@ import {loadIpfsFileAsJson, TIMEFRAMES_TO_SECONDS, timestamp} from "../src/Util.
 
 export async function generateMockComment(parentPostOrComment, plebbit) {
     const commentTime = Date.now() / 1000;
-    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${commentTime}`);
+    const signer = await plebbit.createSigner();
     const comment = await plebbit.createComment({
-        "author": {"displayName": `Mock Author - ${commentTime}`, "address": mockAuthorIpns["id"]},
+        "author": {"displayName": `Mock Author - ${commentTime}`},
+        "signer": signer,
         "content": `Mock comment - ${commentTime}`,
         "postCid": parentPostOrComment.postCid,
         "parentCid": parentPostOrComment.commentCid,
@@ -18,9 +19,10 @@ export async function generateMockComment(parentPostOrComment, plebbit) {
 
 export async function generateMockPost(subplebbitAddress, plebbit) {
     const postStartTestTime = Date.now() / 1000;
-    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${postStartTestTime}`);
+    const signer = await plebbit.createSigner();
     const post = await plebbit.createComment({
-        "author": {"displayName": `Mock Author - ${postStartTestTime}`, "address": mockAuthorIpns["id"]},
+        "author": {"displayName": `Mock Author - ${postStartTestTime}`},
+        "signer": signer,
         "title": `Mock Post - ${postStartTestTime}`,
         "content": `Mock content - ${postStartTestTime}`,
         "subplebbitAddress": subplebbitAddress
@@ -35,9 +37,10 @@ export async function generateMockPostWithRandomTimestamp(subplebbitAddress, ple
     const randomTimeframeIndex = Math.floor(Math.random() * (Object.keys(TIMEFRAMES_TO_SECONDS).length - 1));
     const postTimestamp = timestamp() - (Math.random > 0.5 ? TIMEFRAMES_TO_SECONDS[randomTimeframeIndex] : 0);
     const postTime = Date.now();
-    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${postTime}`);
+    const signer = await plebbit.createSigner();
     const post = await plebbit.createComment({
-        "author": {"displayName": `Mock Author - ${postTime}`, "address": mockAuthorIpns["id"]},
+        "author": {"displayName": `Mock Author - ${postTime}`},
+        "signer": signer,
         "title": `Mock Post - ${postTime}`,
         "content": `Mock content - ${postTime}`,
         "subplebbitAddress": subplebbitAddress,
@@ -53,9 +56,10 @@ export async function generateMockPostWithRandomTimestamp(subplebbitAddress, ple
 
 export async function generateMockVote(parentPostOrComment, vote, subplebbitAddress, plebbit) {
     const voteTime = Date.now() / 1000;
-    const mockAuthorIpns = await plebbit.ipfsClient.key.gen(`Mock User - ${voteTime}`);
+    const signer = await plebbit.createSigner();
     const voteObj = await plebbit.createVote({
-        "author": {"displayName": `Mock Author - ${voteTime}`, "address": mockAuthorIpns["id"]},
+        "author": {"displayName": `Mock Author - ${voteTime}`},
+        "signer": signer,
         "commentCid": parentPostOrComment.commentCid || parentPostOrComment.postCid,
         "vote": vote,
         "subplebbitAddress": subplebbitAddress
