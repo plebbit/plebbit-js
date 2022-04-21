8110c1f3e4ca5543351f41f9530dafc14b0a6a53
Rinse <rinse12@tutanota.com>
2022-04-21 22:38:07 +0000

    No need to export from index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import * as PlebbitClass from "./Plebbit.js";
-import Comment from "./Comment.js";
-import Post from "./Post.js";
-import Vote from "./Vote.js";
 
 export async function Plebbit(options) {
     return new PlebbitClass.Plebbit(options);
 }
-
-export {Comment, Post, Vote};
