fdcc209d8843a7bb6404065adf38d63346da3e01
Rinse <rinse12@tutanota.com>
2022-05-10 08:55:48 +0000

    Export Plebbit as default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as PlebbitClass from "./Plebbit.js";
 
-export async function Plebbit(options) {
+export default async function Plebbit(options) {
     return new PlebbitClass.Plebbit(options);
 }
