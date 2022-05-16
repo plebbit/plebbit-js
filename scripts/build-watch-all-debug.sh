f8d10c979d429bea333d7888657bbc06047cc756
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-05-16 14:19:27 +0000

    fix comment mistake

--- scripts/build-watch-all-debug.sh
+++ scripts/build-watch-all-debug.sh
@@ -2,7 +2,7 @@
 
 # this script is debug what's happening with "npm run build:browser:watch"
 # normally you don't want to look at the browser build as it doesn't
-# give any useful errors and you simply want to run "npm run build:node:watch"
+# give any useful errors and you simply want to run "npm run build:watch"
 # which builds both for node and browser but hides the browser part
 
 xterm -geometry "-0+0" -e "npm run build:node:watch" &
