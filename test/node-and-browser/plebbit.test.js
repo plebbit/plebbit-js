8ecb1783485b6417dff4ce1c081037c3c5c3a284
Rinse <rinse12@tutanota.com>
2022-05-17 09:42:04 +0000

    Migrated old test-Plebbit.js to plebbit.test.js

--- test/node-and-browser/plebbit.test.js
+++ test/node-and-browser/plebbit.test.js
@@ -3,29 +3,40 @@
 const Plebbit = require("../../dist/node");
 const fixtureSigner = require("../fixtures/signers")[0];
 const { expect } = require("chai");
+const signers = require("../fixtures/signers");
+const {
+    loadIpfsFileAsJson,
+    loadIpnsAsJson,
+    removeKeys,
+    removeKeysWithUndefinedValues
+} = require("../../dist/node/util");
+const Post = require("../../dist/node/post");
+const { Subplebbit } = require("../../dist/node/subplebbit");
 
 describe("plebbit (node and browser)", () => {
-    let plebbit, signer;
+    let plebbit, signer, subplebbitSigner;
 
     before(async () => {
         plebbit = await Plebbit();
     });
+    describe("plebbit with default options (cloudflare and pubsubprovider)", async () => {
+        it("has default plebbit options", async () => {
+            expect(plebbit.ipfsGatewayUrl).to.equal("https://cloudflare-ipfs.com");
+            expect(plebbit.pubsubHttpClientOptions).to.equal("https://pubsubprovider.xyz/api/v0");
 
-    it("has default plebbit options", async () => {
-        expect(plebbit.ipfsGatewayUrl).to.equal("https://cloudflare-ipfs.com");
-        expect(plebbit.pubsubHttpClientOptions).to.equal("https://pubsubprovider.xyz/api/v0");
-
-        // no dataPath in brower
-        if (typeof window === "undefined") {
-            expect(plebbit.dataPath).to.match(/\.plebbit$/);
-        } else {
-            expect(plebbit.dataPath).to.equal(undefined);
-        }
+            // no dataPath in browser
+            if (typeof window === "undefined") {
+                expect(plebbit.dataPath).to.match(/\.plebbit$/);
+            } else {
+                expect(plebbit.dataPath).to.equal(undefined);
+            }
+        });
     });
 
     describe("plebbit.createSigner", () => {
         before(async () => {
             signer = await plebbit.createSigner();
+            subplebbitSigner = await plebbit.createSigner(signers[0]);
         });
 
         it("without private key argument", async () => {
@@ -53,6 +64,58 @@ describe("plebbit (node and browser)", () => {
             expect(signer.publicKey).to.equal(signer2.publicKey);
             expect(signer.address).to.equal(signer2.address);
             expect(signer.type).to.equal(signer2.type);
+        });
+    });
+
+    describe("plebbit.getComment", async () => {
+        before(async () => {
+            plebbit = await Plebbit({ ipfsHttpClientOptions: "http://localhost:5001/api/v0" });
+        });
+        it("loads post correctly", async () => {
+            const subplebbit = await plebbit.getSubplebbit(subplebbitSigner.address);
+            await subplebbit.update();
+            expect(subplebbit).to.have.property("latestPostCid"); // Part of setting up test-server.js to publish a test post
+            const expectedPostProps = await loadIpfsFileAsJson(subplebbit.latestPostCid, plebbit);
+            const expectedPost = await plebbit.createComment({
+                cid: subplebbit.latestPostCid,
+                postCid: subplebbit.latestPostCid,
+                ...expectedPostProps
+            });
+            expect(expectedPost.getType()).to.equal("post");
+            await expectedPost.update();
+            const loadedPost = await plebbit.getComment(subplebbit.latestPostCid);
+            expect(loadedPost.getType()).to.equal("post");
+            await loadedPost.update();
+
+            expect(JSON.stringify(loadedPost)).to.equal(JSON.stringify(expectedPost));
+        });
+
+        it("loads comment correctly", async () => {
+            const subplebbit = await plebbit.getSubplebbit(subplebbitSigner.address);
+            await subplebbit.update();
+            expect(subplebbit).to.have.property("latestPostCid"); // Part of setting up test-server.js to publish a test post
+            const comment = subplebbit?.posts?.pages?.hot?.comments[0]?.replies?.pages?.topAll?.comments[0];
+            const expectedCommentProps = await loadIpfsFileAsJson(comment.cid, plebbit);
+            const expectedComment = await plebbit.createComment({ cid: comment.cid, ...expectedCommentProps });
+            expect(expectedComment.getType()).to.equal("comment");
+            await expectedComment.update();
+
+            const loadedComment = await plebbit.getComment(comment.cid);
+            expect(loadedComment.getType()).to.equal("comment");
+            await loadedComment.update();
+
+            expect(JSON.stringify(loadedComment)).to.equal(JSON.stringify(expectedComment));
+        });
+    });
+
+    describe("plebbit.getSubplebbit", async () => {
+        it("loads subplebbit correctly", async () => {
+            const _subplebbitIpns = await loadIpnsAsJson(subplebbitSigner.address, plebbit);
+            const expectedSubplebbit = await plebbit.createSubplebbit(_subplebbitIpns);
+            expect(expectedSubplebbit instanceof Subplebbit).to.equal(true);
+            const loadedSubplebbit = await plebbit.getSubplebbit(subplebbitSigner.address);
+            expect(expectedSubplebbit instanceof Subplebbit).to.equal(true);
+            expect(JSON.stringify(loadedSubplebbit)).to.equal(JSON.stringify(expectedSubplebbit));
         });
     });
 });
