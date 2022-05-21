a7b62e9d8b2850237307b1d6d909889364dafb4c
Rinse <rinse12@tutanota.com>
2022-05-21 17:36:15 +0000

    Updated test-util.js to use promises instead of async keyword

--- test/test-util.js
+++ test/test-util.js
@@ -1,34 +1,49 @@
-async function generateMockPost(subplebbitAddress, plebbit, postSigner = undefined) {
-    const postStartTestTime = Date.now() / 1000;
-    const signer = postSigner || (await plebbit.createSigner());
-    const post = await plebbit.createComment({
-        author: { displayName: `Mock Author - ${postStartTestTime}` },
-        signer: signer,
-        title: `Mock Post - ${postStartTestTime}`,
-        content: `Mock content - ${postStartTestTime}`,
-        subplebbitAddress: subplebbitAddress
+// Be careful of using async keyword here, it triggers babel to parse this file as a module for some reason
+// Always use promises
+
+function generateMockPost(subplebbitAddress, plebbit) {
+    return new Promise((resolve) => {
+        const postStartTestTime = Date.now() / 1000;
+        plebbit.createSigner().then((signer) => {
+            plebbit
+                .createComment({
+                    author: { displayName: `Mock Author - ${postStartTestTime}` },
+                    signer: signer,
+                    title: `Mock Post - ${postStartTestTime}`,
+                    content: `Mock content - ${postStartTestTime}`,
+                    subplebbitAddress: subplebbitAddress
+                })
+                .then((post) => {
+                    post.once("challenge", (challengeMsg) => {
+                        post.publishChallengeAnswers(undefined);
+                    });
+                    resolve(post);
+                });
+        });
     });
-    post.once("challenge", (challengeMsg) => {
-        post.publishChallengeAnswers(undefined);
-    });
-    return post;
 }
 
-async function generateMockComment(parentPostOrComment, plebbit, commentSigner = undefined) {
-    const commentTime = Date.now() / 1000;
-    const signer = commentSigner || (await plebbit.createSigner());
-    const comment = await plebbit.createComment({
-        author: { displayName: `Mock Author - ${commentTime}` },
-        signer: signer,
-        content: `Mock comment - ${commentTime}`,
-        postCid: parentPostOrComment.postCid || parentPostOrComment.cid,
-        parentCid: parentPostOrComment.cid,
-        subplebbitAddress: parentPostOrComment.subplebbitAddress
+function generateMockComment(parentPostOrComment, plebbit) {
+    return new Promise((resolve) => {
+        const commentTime = Date.now() / 1000;
+        plebbit.createSigner().then((signer) => {
+            plebbit
+                .createComment({
+                    author: { displayName: `Mock Author - ${commentTime}` },
+                    signer: signer,
+                    content: `Mock comment - ${commentTime}`,
+                    postCid: parentPostOrComment.postCid || parentPostOrComment.cid,
+                    parentCid: parentPostOrComment.cid,
+                    subplebbitAddress: parentPostOrComment.subplebbitAddress
+                })
+                .then((comment) => {
+                    comment.once("challenge", (challengeMsg) => {
+                        comment.publishChallengeAnswers(undefined);
+                    });
+                    resolve(comment);
+                });
+        });
     });
-    comment.once("challenge", (challengeMsg) => {
-        comment.publishChallengeAnswers(undefined);
-    });
-    return comment;
 }
 
 module.exports = { generateMockPost, generateMockComment };
