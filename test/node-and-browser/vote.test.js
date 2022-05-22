1d50b1097dd6bf3e84c12382a43f70bcf89c470c
Rinse <rinse12@tutanota.com>
2022-05-22 14:15:51 +0000

    Migrated test-Vote.js to vote.test.js

---
+++ test/node-and-browser/vote.test.js
@@ -0,0 +1,144 @@
+const Plebbit = require("../../dist/node");
+const { expect } = require("chai");
+const signers = require("../fixtures/signers");
+const { generateMockVote } = require("../../dist/node/test-util");
+const { timestamp } = require("../../dist/node/util");
+
+const subplebbitAddress = signers[0].address;
+
+let plebbit, subplebbit, postToVote;
+
+const previousVotes = [];
+
+describe("Test Vote", async () => {
+    before(async () => {
+        plebbit = await Plebbit({
+            ipfsHttpClientOptions: "http://localhost:5001/api/v0",
+            pubsubHttpClientOptions: `http://localhost:5002/api/v0`
+        });
+        subplebbit = await plebbit.getSubplebbit(subplebbitAddress);
+        await subplebbit.update(5000);
+        postToVote = await plebbit.getComment(subplebbit.latestPostCid);
+    });
+
+    it("Can upvote a comment", async () => {
+        return new Promise(async (resolve, reject) => {
+            const vote = await generateMockVote(postToVote, 1, plebbit);
+
+            await (await generateMockVote(postToVote, 1, plebbit)).publish(); // This vote is added just to start an "update" event and make sure originalUpvoteCount down below is accurate
+
+            subplebbit.once("update", async (updatedSubplebbit) => {
+                await postToVote.update(5000);
+                const originalUpvote = postToVote.upvoteCount;
+                await vote.publish();
+                postToVote.once("update", async (updatedPost) => {
+                    expect(updatedPost.upvoteCount).to.be.equal(originalUpvote + 1);
+                    previousVotes.push(vote);
+                    resolve();
+                });
+            });
+        });
+    });
+
+    it("Throws an error when vote is duplicated", async () => {
+        return new Promise(async (resolve, reject) => {
+            const vote = await plebbit.createVote({
+                ...previousVotes[0].toJSON(),
+                signer: previousVotes[0].signer,
+                timestamp: timestamp()
+            });
+            await vote.publish();
+            vote.once("challengeverification", async ([challengeVerificationMsg]) => {
+                expect(challengeVerificationMsg.challengePassed).to.be.false;
+                expect(challengeVerificationMsg.reason).to.be.a("string").with.length.at.least(1);
+                resolve();
+            });
+        });
+    });
+
+    it("Can change upvote to downvote", async () => {
+        return new Promise(async (resolve, reject) => {
+            await postToVote.update(5000);
+            const originalUpvote = postToVote.upvoteCount;
+            const originalDownvote = postToVote.downvoteCount;
+            const vote = await plebbit.createVote({
+                ...previousVotes[0].toJSON(),
+                signer: previousVotes[0].signer,
+                vote: -1,
+                timestamp: timestamp()
+            });
+            await vote.publish();
+            postToVote.once("update", async (updatedPost) => {
+                expect(updatedPost.upvoteCount).to.equal(originalUpvote - 1);
+                expect(updatedPost.downvoteCount).to.equal(originalDownvote + 1);
+                resolve();
+            });
+        });
+    });
+
+    it("Can cancel a comment downvote", async () => {
+        return new Promise(async (resolve, reject) => {
+            const originalDownvote = postToVote.downvoteCount;
+            const vote = await plebbit.createVote({
+                ...previousVotes[0].toJSON(),
+                signer: previousVotes[0].signer,
+                vote: 0,
+                timestamp: timestamp()
+            });
+            await vote.publish();
+            postToVote.once("update", async (updatedPost) => {
+                expect(updatedPost.downvoteCount).to.equal(originalDownvote - 1);
+                resolve();
+            });
+        });
+    });
+
+    it("Can downvote a comment", async () => {
+        return new Promise(async (resolve, reject) => {
+            const originalDownvote = postToVote.downvoteCount;
+            const vote = await generateMockVote(postToVote, -1, plebbit);
+            await vote.publish();
+            postToVote.once("update", async (updatedPost) => {
+                expect(updatedPost.downvoteCount).to.equal(originalDownvote + 1);
+                previousVotes.push(vote);
+                resolve();
+            });
+        });
+    });
+
+    it("Can change downvote to upvote", async () => {
+        return new Promise(async (resolve, reject) => {
+            const originalUpvote = postToVote.upvoteCount;
+            const originalDownvote = postToVote.downvoteCount;
+            const vote = await plebbit.createVote({
+                ...previousVotes[1].toJSON(),
+                signer: previousVotes[1].signer,
+                vote: 1,
+                timestamp: timestamp()
+            });
+            await vote.publish();
+            postToVote.once("update", async (updatedPost) => {
+                expect(updatedPost.upvoteCount).to.equal(originalUpvote + 1, "Failed to update upvote count");
+                expect(updatedPost.downvoteCount).to.equal(originalDownvote - 1, "Failed to update downvote count");
+                resolve();
+            });
+        });
+    });
+
+    it("Can cancel a comment upvote", async () => {
+        return new Promise(async (resolve, reject) => {
+            const originalUpvote = postToVote.upvoteCount;
+            const vote = await plebbit.createVote({
+                ...previousVotes[1].toJSON(),
+                signer: previousVotes[1].signer,
+                vote: 0,
+                timestamp: timestamp()
+            });
+            await vote.publish();
+            postToVote.once("update", async (updatedPost) => {
+                expect(updatedPost.upvoteCount).to.equal(originalUpvote - 1);
+                resolve();
+            });
+        });
+    });
+});
