534803336a5c02aaa936b76ccd516f9f0d8b8779
Rinse <rinse12@tutanota.com>
2022-05-22 10:58:02 +0000

    migrated test-Subplebbit.js to subplebbit.test.js

---
+++ test/node/subplebbit.test.js
@@ -0,0 +1,44 @@
+const Plebbit = require("../../dist/node");
+const { expect } = require("chai");
+
+let plebbit;
+let subplebbit;
+
+describe("subplebbit", async () => {
+    before(async () => {
+        plebbit = await Plebbit({
+            ipfsHttpClientOptions: "http://localhost:5001/api/v0",
+            pubsubHttpClientOptions: `http://localhost:5002/api/v0`
+        });
+    });
+
+    it("create new subplebbit", async function () {
+        const signer = await plebbit.createSigner();
+        subplebbit = await plebbit.createSubplebbit({
+            signer: signer,
+            title: `Test subplebbit - ${Date.now() / 1000}`
+        });
+
+        await subplebbit.start();
+        expect(subplebbit.address).to.be.a("string");
+        // Should have address now
+        const loadedSubplebbit = await plebbit.getSubplebbit(subplebbit.address);
+        expect(subplebbit.toJSON()).to.deep.equal(loadedSubplebbit.toJSON());
+    });
+
+    it("subplebbit.edit", async () => {
+        return new Promise(async (resolve) => {
+            const newTitle = `New title to test subplebbit.edit - ${Date.now()}`;
+            const newDescription = `New description to test subplebbit.edit - ${Date.now()}`;
+            const newProps = { title: newTitle, description: newDescription };
+            await subplebbit.edit(newProps);
+
+            subplebbit.once("update", async () => {
+                expect(subplebbit.title).to.equal(newTitle);
+                expect(subplebbit.description, newDescription);
+                resolve();
+            });
+            await subplebbit.update();
+        });
+    });
+});
