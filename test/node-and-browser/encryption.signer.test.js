259acdda41f069e29aae779fbc68fcd0999f484b
Rinse <rinse12@tutanota.com>
2022-07-08 22:48:42 +0000

    split signer.test.js into two files

---
+++ test/node-and-browser/encryption.signer.test.js
@@ -0,0 +1,188 @@
+const Plebbit = require("../../dist/node");
+const fixtureSigners = require("../fixtures/signers");
+const authorSignerFixture = fixtureSigners[1];
+const fixtureComment = require("../fixtures/publications").comment;
+const { encrypt, decrypt } = require("../../dist/node/signer");
+const {
+    encryptBufferRsa,
+    decryptBufferRsa,
+    generateKeyAesCbc,
+    encryptStringAesCbc,
+    decryptStringAesCbc
+} = require("../../dist/node/signer/encryption");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const { expect } = chai;
+const { toString } = require("uint8arrays/to-string");
+const { fromString } = require("uint8arrays/from-string");
+
+describe("encryption", () => {
+    let plebbit, authorSigner;
+
+    before(async function () {
+        plebbit = await Plebbit({
+            ipfsHttpClientOptions: "http://localhost:5001/api/v0",
+            pubsubHttpClientOptions: `http://localhost:5002/api/v0`
+        });
+        authorSigner = await plebbit.createSigner({ privateKey: authorSignerFixture.privateKey, type: "rsa" });
+    });
+
+    describe("encrypt and decrypt aes-cbc key buffer with rsa", () => {
+        // key must be 16 bytes
+        const key = fromString("1111111111111111");
+        let encryptedKey;
+
+        before(async () => {
+            encryptedKey = await encryptBufferRsa(key, authorSignerFixture.publicKey);
+        });
+
+        it("encrypted key is string of correct length", () => {
+            expect(encryptedKey).to.be.a("string");
+            expect(encryptedKey).to.be.of.length(342);
+        });
+
+        it("encrypted key can be decrypted", async () => {
+            const decryptedKey = await decryptBufferRsa(encryptedKey, authorSignerFixture.privateKey);
+            expect(toString(key)).to.equal(toString(decryptedKey));
+        });
+    });
+
+    describe("generate, encrypt and decrypt aes-cbc key buffer with rsa", () => {
+        let key, encryptedKey;
+
+        before(async () => {
+            key = await generateKeyAesCbc();
+            encryptedKey = await encryptBufferRsa(key, authorSignerFixture.publicKey);
+        });
+
+        it("generated key is Uint8Array of correct length", () => {
+            expect(key).to.be.instanceof(Uint8Array);
+            expect(key).to.be.of.length(16);
+        });
+
+        it("encrypted key is string of correct length", () => {
+            expect(encryptedKey).to.be.a("string");
+            expect(encryptedKey).to.be.of.length(342);
+        });
+
+        it("encrypted key can be decrypted", async () => {
+            const decryptedKey = await decryptBufferRsa(encryptedKey, authorSignerFixture.privateKey);
+            expect(toString(key)).to.equal(toString(decryptedKey));
+        });
+    });
+
+    describe("encrypt and decrypt string with aes-cbc", async () => {
+        describe("generated key", () => {
+            let key, encryptedString;
+            before(async () => {
+                key = await generateKeyAesCbc();
+                encryptedString = await encryptStringAesCbc(JSON.stringify(fixtureComment), key);
+            });
+
+            it("encrypted string is not empty", async () => {
+                expect(encryptedString).to.be.a("string");
+                expect(encryptedString).to.not.be.empty;
+            });
+
+            it("encrypted string can be decrypted", async () => {
+                const decryptedString = await decryptStringAesCbc(encryptedString, key);
+                expect(decryptedString).to.be.a("string");
+                expect(decryptedString).to.not.be.empty;
+                expect(JSON.stringify(fixtureComment)).to.equal(decryptedString);
+            });
+        });
+
+        describe("fixed key", () => {
+            // key must be 16 bytes for aes-cbc 128
+            const key = fromString("1111111111111111");
+
+            describe("encrypt the word 'string'", () => {
+                const string = "string";
+                let encryptedString;
+                before(async () => {
+                    encryptedString = await encryptStringAesCbc(string, key);
+                });
+
+                it("encrypted string is not empty", async () => {
+                    const encryptedString = await encryptStringAesCbc(string, key);
+                    expect(encryptedString).to.be.a("string");
+                    expect(encryptedString).to.not.be.empty;
+
+                    // base 64 aes-cbc 128 encrypted "string" with key "1111111111111111"
+                    // should always return this value
+                    expect(encryptedString).to.equal("1OXdTy9YFztlOz3lo4NXxw");
+                });
+
+                it("encrypted string can be decrypted", async () => {
+                    const decryptedString = await decryptStringAesCbc(encryptedString, key);
+                    expect(decryptedString).to.be.a("string");
+                    expect(decryptedString).to.not.be.empty;
+                    expect(string).to.equal(decryptedString);
+                });
+            });
+
+            describe("encrypt an emoji", () => {
+                const emoji = "🤡";
+                let encryptedEmoji;
+                before(async () => {
+                    encryptedEmoji = await encryptStringAesCbc(emoji, key);
+                });
+
+                it("encrypted emoji is not empty", async () => {
+                    const encryptedEmoji = await encryptStringAesCbc(emoji, key);
+                    expect(encryptedEmoji).to.be.a("string");
+                    expect(encryptedEmoji).to.not.be.empty;
+
+                    // base 64 aes-cbc 128 encrypted "🤡" with key "1111111111111111"
+                    // should always return this value
+                    expect(encryptedEmoji).to.equal("ornXmW+KABncGYANn74jjQ");
+                });
+
+                it("encrypted emoji can be decrypted", async () => {
+                    const decryptedEmoji = await decryptStringAesCbc(encryptedEmoji, key);
+                    expect(decryptedEmoji).to.be.a("string");
+                    expect(decryptedEmoji).to.not.be.empty;
+                    expect(emoji).to.equal(decryptedEmoji);
+                });
+            });
+        });
+    });
+
+    describe("encrypt and decrypt publication with rsa + aes-cbc", () => {
+        let encryptedPublication;
+
+        before(async () => {
+            encryptedPublication = await encrypt(JSON.stringify(fixtureComment), authorSignerFixture.publicKey);
+        });
+
+        it("encrypted publication has expected properties", async () => {
+            expect(encryptedPublication).to.not.equal(undefined);
+            expect(encryptedPublication.encrypted).to.be.a("string");
+            expect(encryptedPublication.encrypted).to.not.be.empty;
+            expect(encryptedPublication.encryptedKey).to.be.a("string");
+            expect(encryptedPublication.encryptedKey).to.not.be.empty;
+            expect(encryptedPublication.type).to.equal("aes-cbc");
+        });
+
+        it("encrypted publication can be decrypted", async () => {
+            const decryptedPublicationString = await decrypt(
+                encryptedPublication.encrypted,
+                encryptedPublication.encryptedKey,
+                authorSignerFixture.privateKey
+            );
+            expect(encryptedPublication.encrypted).to.be.a("string");
+            expect(encryptedPublication.encrypted).to.not.be.empty;
+            let decryptedPublication;
+            try {
+                decryptedPublication = JSON.parse(decryptedPublicationString);
+            } catch (e) {
+                throw Error(`decrypted publication isn't valid JSON: ${e.message}`);
+            }
+            expect(decryptedPublication.subplebbitAddress).to.equal(fixtureComment.subplebbitAddress);
+            expect(decryptedPublication.timestamp).to.equal(fixtureComment.timestamp);
+            expect(decryptedPublication.content).to.equal(fixtureComment.content);
+            expect(decryptedPublication.author?.address).to.equal(fixtureComment.author.address);
+        });
+    });
+});
--- test/node-and-browser/signer.test.js
+++ test/node-and-browser/signer.test.js
@@ -2,14 +2,7 @@ const Plebbit = require("../../dist/node");
 const fixtureSigners = require("../fixtures/signers");
 const authorSignerFixture = fixtureSigners[1];
 const fixtureComment = require("../fixtures/publications").comment;
-const { signPublication, verifyPublication, encrypt, decrypt } = require("../../dist/node/signer");
-const {
-    encryptBufferRsa,
-    decryptBufferRsa,
-    generateKeyAesCbc,
-    encryptStringAesCbc,
-    decryptStringAesCbc
-} = require("../../dist/node/signer/encryption");
+const { signPublication, verifyPublication } = require("../../dist/node/signer");
 const { signBufferRsa, verifyBufferRsa } = require("../../dist/node/signer/signatures");
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
@@ -79,7 +72,7 @@ describe("signer (node and browser)", async () => {
     });
 
     describe("sign and verify publication", () => {
-        let authorSignature, randomSignature, signedPublication;
+        let authorSignature, signedPublication;
 
         before(async () => {
             authorSignature = await signPublication(fixtureComment, authorSigner, plebbit);
@@ -143,166 +136,6 @@ describe("signer (node and browser)", async () => {
             const [validity, failedVerificationReason] = await verifyPublication(comment);
             expect(validity).to.be.true;
             expect(failedVerificationReason).to.be.undefined;
-        });
-    });
-
-    describe("encryption", () => {
-        describe("encrypt and decrypt aes-cbc key buffer with rsa", () => {
-            // key must be 16 bytes
-            const key = fromString("1111111111111111");
-            let encryptedKey;
-
-            before(async () => {
-                encryptedKey = await encryptBufferRsa(key, authorSignerFixture.publicKey);
-            });
-
-            it("encrypted key is string of correct length", () => {
-                expect(encryptedKey).to.be.a("string");
-                expect(encryptedKey).to.be.of.length(342);
-            });
-
-            it("encrypted key can be decrypted", async () => {
-                const decryptedKey = await decryptBufferRsa(encryptedKey, authorSignerFixture.privateKey);
-                expect(toString(key)).to.equal(toString(decryptedKey));
-            });
-        });
-
-        describe("generate, encrypt and decrypt aes-cbc key buffer with rsa", () => {
-            let key, encryptedKey;
-
-            before(async () => {
-                key = await generateKeyAesCbc();
-                encryptedKey = await encryptBufferRsa(key, authorSignerFixture.publicKey);
-            });
-
-            it("generated key is Uint8Array of correct length", () => {
-                expect(key).to.be.instanceof(Uint8Array);
-                expect(key).to.be.of.length(16);
-            });
-
-            it("encrypted key is string of correct length", () => {
-                expect(encryptedKey).to.be.a("string");
-                expect(encryptedKey).to.be.of.length(342);
-            });
-
-            it("encrypted key can be decrypted", async () => {
-                const decryptedKey = await decryptBufferRsa(encryptedKey, authorSignerFixture.privateKey);
-                expect(toString(key)).to.equal(toString(decryptedKey));
-            });
-        });
-
-        describe("encrypt and decrypt string with aes-cbc", async () => {
-            describe("generated key", () => {
-                let key, encryptedString;
-                before(async () => {
-                    key = await generateKeyAesCbc();
-                    encryptedString = await encryptStringAesCbc(JSON.stringify(fixtureComment), key);
-                });
-
-                it("encrypted string is not empty", async () => {
-                    expect(encryptedString).to.be.a("string");
-                    expect(encryptedString).to.not.be.empty;
-                });
-
-                it("encrypted string can be decrypted", async () => {
-                    const decryptedString = await decryptStringAesCbc(encryptedString, key);
-                    expect(decryptedString).to.be.a("string");
-                    expect(decryptedString).to.not.be.empty;
-                    expect(JSON.stringify(fixtureComment)).to.equal(decryptedString);
-                });
-            });
-
-            describe("fixed key", () => {
-                // key must be 16 bytes for aes-cbc 128
-                const key = fromString("1111111111111111");
-
-                describe("encrypt the word 'string'", () => {
-                    const string = "string";
-                    let encryptedString;
-                    before(async () => {
-                        encryptedString = await encryptStringAesCbc(string, key);
-                    });
-
-                    it("encrypted string is not empty", async () => {
-                        const encryptedString = await encryptStringAesCbc(string, key);
-                        expect(encryptedString).to.be.a("string");
-                        expect(encryptedString).to.not.be.empty;
-
-                        // base 64 aes-cbc 128 encrypted "string" with key "1111111111111111"
-                        // should always return this value
-                        expect(encryptedString).to.equal("1OXdTy9YFztlOz3lo4NXxw");
-                    });
-
-                    it("encrypted string can be decrypted", async () => {
-                        const decryptedString = await decryptStringAesCbc(encryptedString, key);
-                        expect(decryptedString).to.be.a("string");
-                        expect(decryptedString).to.not.be.empty;
-                        expect(string).to.equal(decryptedString);
-                    });
-                });
-
-                describe("encrypt an emoji", () => {
-                    const emoji = "🤡";
-                    let encryptedEmoji;
-                    before(async () => {
-                        encryptedEmoji = await encryptStringAesCbc(emoji, key);
-                    });
-
-                    it("encrypted emoji is not empty", async () => {
-                        const encryptedEmoji = await encryptStringAesCbc(emoji, key);
-                        expect(encryptedEmoji).to.be.a("string");
-                        expect(encryptedEmoji).to.not.be.empty;
-
-                        // base 64 aes-cbc 128 encrypted "🤡" with key "1111111111111111"
-                        // should always return this value
-                        expect(encryptedEmoji).to.equal("ornXmW+KABncGYANn74jjQ");
-                    });
-
-                    it("encrypted emoji can be decrypted", async () => {
-                        const decryptedEmoji = await decryptStringAesCbc(encryptedEmoji, key);
-                        expect(decryptedEmoji).to.be.a("string");
-                        expect(decryptedEmoji).to.not.be.empty;
-                        expect(emoji).to.equal(decryptedEmoji);
-                    });
-                });
-            });
-        });
-
-        describe("encrypt and decrypt publication with rsa + aes-cbc", () => {
-            let encryptedPublication;
-
-            before(async () => {
-                encryptedPublication = await encrypt(JSON.stringify(fixtureComment), authorSignerFixture.publicKey);
-            });
-
-            it("encrypted publication has expected properties", async () => {
-                expect(encryptedPublication).to.not.equal(undefined);
-                expect(encryptedPublication.encrypted).to.be.a("string");
-                expect(encryptedPublication.encrypted).to.not.be.empty;
-                expect(encryptedPublication.encryptedKey).to.be.a("string");
-                expect(encryptedPublication.encryptedKey).to.not.be.empty;
-                expect(encryptedPublication.type).to.equal("aes-cbc");
-            });
-
-            it("encrypted publication can be decrypted", async () => {
-                const decryptedPublicationString = await decrypt(
-                    encryptedPublication.encrypted,
-                    encryptedPublication.encryptedKey,
-                    authorSignerFixture.privateKey
-                );
-                expect(encryptedPublication.encrypted).to.be.a("string");
-                expect(encryptedPublication.encrypted).to.not.be.empty;
-                let decryptedPublication;
-                try {
-                    decryptedPublication = JSON.parse(decryptedPublicationString);
-                } catch (e) {
-                    throw Error(`decrypted publication isn't valid JSON: ${e.message}`);
-                }
-                expect(decryptedPublication.subplebbitAddress).to.equal(fixtureComment.subplebbitAddress);
-                expect(decryptedPublication.timestamp).to.equal(fixtureComment.timestamp);
-                expect(decryptedPublication.content).to.equal(fixtureComment.content);
-                expect(decryptedPublication.author?.address).to.equal(fixtureComment.author.address);
-            });
         });
     });
 });
