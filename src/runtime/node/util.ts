fec68203915ce32ef4aec4da7ae4efe8193c764c
Rinse <rinse12@tutanota.com>
2022-07-13 23:10:19 +0000

    mkdir subplebbits path before listing subplebbits

--- src/runtime/node/util.ts
+++ src/runtime/node/util.ts
@@ -1,11 +1,16 @@
 import path from "path";
 import { promises as fs } from "fs";
 import { Plebbit } from "../../plebbit";
+import assert from "assert";
 
 export const getDefaultDataPath = () => path.join(process.cwd(), ".plebbit");
 
 export const listSubplebbits = async (plebbit: Plebbit) => {
+    assert(plebbit.dataPath, "plebbit.dataPath is needed to list subplebbits");
     const subplebbitsPath = path.join(plebbit.dataPath, "subplebbits");
+
+    await fs.mkdir(subplebbitsPath, { recursive: true });
+
     const addresses = await fs.readdir(subplebbitsPath);
     return addresses;
 };
