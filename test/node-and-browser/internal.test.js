662363106c79f25402601e1b1aac4254881947f9
Rinse <rinse12@tutanota.com>
2022-07-13 22:57:23 +0000

    Fixed issues with loading files via ipfs tests

--- test/node-and-browser/internal.test.js
+++ test/node-and-browser/internal.test.js
@@ -6,32 +6,29 @@ chai.use(chaiAsPromised);
 const { expect, assert } = chai;
 
 describe("Test util functions", async () => {
-    let plebbit;
+    let plebbit, gatewayPlebbit;
     before(async () => {
         plebbit = await Plebbit({
             ipfsHttpClientOptions: "http://localhost:5001/api/v0",
             pubsubHttpClientOptions: `http://localhost:5002/api/v0`
         });
+        gatewayPlebbit = await Plebbit({
+            ipfsGatewayUrl: "http://127.0.0.1:8080"
+        });
     });
     describe("loadIpfsAsJson", async () => {
-        it("Throws if provided with invalid cid", async () =>
-            new Promise(async (resolve, reject) => {
-                const gibberishCid = "12345";
-                try {
-                    await loadIpfsFileAsJson(gibberishCid, plebbit);
-                    reject();
-                } catch (e) {
-                    resolve();
-                }
-            }));
+        it("Throws if provided with invalid cid", async () => {
+            const gibberishCid = "12345";
+
+            await assert.isRejected(loadIpfsFileAsJson(gibberishCid, plebbit), "invalid path");
+            await assert.isRejected(loadIpfsFileAsJson(gibberishCid, gatewayPlebbit), "Status code 404");
+        });
         it("Loads an ipfs file under 1mb as JSON correctly", async () => {
             const jsonFileTest = { 123: "123" };
             const cid = (await plebbit.ipfsClient.add(JSON.stringify(jsonFileTest))).path;
             let jsonFileLoaded = await loadIpfsFileAsJson(cid, plebbit);
             expect(jsonFileLoaded).to.deep.equal(jsonFileTest);
-            const gatewayPlebbit = await Plebbit({
-                ipfsGatewayUrl: "http://localhost:8080"
-            });
+
             jsonFileLoaded = await loadIpfsFileAsJson(cid, gatewayPlebbit);
             expect(jsonFileLoaded).to.deep.equal(jsonFileTest);
         });
@@ -41,38 +38,22 @@ describe("Test util functions", async () => {
 
             const cid = (await plebbit.ipfsClient.add(JSON.stringify(twoMbObject))).path; // Cid of a file with over 1mb size
 
-            await assert.isRejected(loadIpfsFileAsJson(cid, plebbit));
-            await assert.isRejected(
-                loadIpfsFileAsJson(
-                    cid,
-                    await Plebbit({
-                        ipfsGatewayUrl: "http://localhost:8080"
-                    })
-                )
-            );
+            await assert.isRejected(loadIpfsFileAsJson(cid, plebbit), "Unexpected end of JSON input");
+            await assert.isRejected(loadIpfsFileAsJson(cid, gatewayPlebbit), "content size");
         });
     });
 
     describe("loadIpnsAsJson", async () => {
-        it("Throws if provided with invalid ipns", async () =>
-            new Promise(async (resolve, reject) => {
-                const gibberishIpns = "12345";
-                try {
-                    await loadIpnsAsJson(gibberishIpns, plebbit);
-                    reject();
-                } catch (e) {
-                    resolve();
-                }
-            }));
+        it("Throws if provided with invalid ipns", async () => {
+            const gibberishIpns = "12345";
+            await assert.isRejected(loadIpnsAsJson(gibberishIpns, plebbit), "could not resolve name");
+        });
         it("Loads an IPNS file as JSON correctly", async () => {
             const jsonFileTest = { 1234: "1234" };
             const cid = (await plebbit.ipfsClient.add(JSON.stringify(jsonFileTest))).path;
             const jsonFileAsIpns = await plebbit.ipfsClient.name.publish(cid, { allowOffline: true });
             let jsonFileLoaded = await loadIpnsAsJson(jsonFileAsIpns.name, plebbit);
             expect(jsonFileLoaded).to.deep.equal(jsonFileTest);
-            const gatewayPlebbit = await Plebbit({
-                ipfsGatewayUrl: "http://localhost:8080"
-            });
             jsonFileLoaded = await loadIpnsAsJson(jsonFileAsIpns.name, gatewayPlebbit);
             expect(jsonFileLoaded).to.deep.equal(jsonFileTest);
         });
@@ -89,11 +70,7 @@ describe("Test util functions", async () => {
                 })
             ).name;
 
-            const gatewayPlebbit = await Plebbit({
-                ipfsGatewayUrl: "http://localhost:8080"
-            });
-
-            await assert.isRejected(loadIpnsAsJson(ipns, gatewayPlebbit));
+            await assert.isRejected(loadIpnsAsJson(ipns, gatewayPlebbit), "content size");
         });
     });
 });
