127965319c24b46a84f38a16b217ee0091df9aac
Rinse <rinse12@tutanota.com>
2022-07-23 15:25:37 +0000

    Added rest of author props (previousCommentCid, wallets, flair, banExpiresAt)

--- src/author.ts
+++ src/author.ts
@@ -1,20 +1,39 @@
-import { Nft } from "./types";
+import { AuthorType, Flair, Nft, Wallet } from "./types";
+import { parseJsonIfString } from "./util";
 
-class Author {
+class Author implements AuthorType {
+    address: string;
+    previousCommentCid?: string; // linked list of the author's comments
     displayName?: string;
+    wallets?: { [chainTicker: string]: Wallet };
     avatar?: Nft;
-    address?: string;
+    flair?: Flair;
+    banExpiresAt?: number;
 
-    constructor(props: Author) {
-        Object.assign(this, props);
+    constructor(props: AuthorType) {
+        this.address = props.address;
+        this.previousCommentCid = props.previousCommentCid;
+        this.displayName = props.displayName;
+        this.wallets = props.wallets;
+        this.avatar = props.avatar;
+        this.flair = parseJsonIfString(props.flair);
+        this.banExpiresAt = props.banExpiresAt;
     }
 
-    toJSON?() {
-        return { address: this.address, displayName: this.displayName, avatar: this.avatar };
+    toJSON(): AuthorType {
+        return {
+            address: this.address,
+            previousCommentCid: this.previousCommentCid,
+            displayName: this.displayName,
+            wallets: this.wallets,
+            avatar: this.avatar,
+            flair: this.flair,
+            banExpiresAt: this.banExpiresAt
+        };
     }
 
-    toJSONForDb?() {
-        return { address: this.address };
+    toJSONForDb() {
+        return { address: this.address, banExpiresAt: this.banExpiresAt, flair: this.flair };
     }
 }
 
