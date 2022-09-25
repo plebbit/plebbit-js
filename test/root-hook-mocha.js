f57a5b0ed43d2294b79751c3fcdc11194e60e310
Rinse <rinse12@tutanota.com>
2022-09-25 11:38:00 +0000

    no need for mocha hooks

--- test/root-hook-mocha.js
+++ test/root-hook-mocha.js
@@ -1,21 +1,2 @@
-const Plebbit = require("../dist/node");
-
-const setupNativeFunction = () => {
-    const isElectron = globalThis["navigator"]?.userAgent?.includes("Electron");
-    if (isElectron) {
-        // If in electron env, set native functiosn
-        console.log(window.plebbitJsNativeFunctions);
-        console.log(`Detected Electron env. Will set native functions`);
-        Plebbit.setNativeFunctions(window.plebbitJsNativeFunctions);
-    } else {
-        console.log(`Detected non native Electron env`);
-    }
-};
-
 // Root Mocha hook.
-exports.mochaHooks = {
-    beforeEach(done) {
-        setupNativeFunction();
-        done();
-    }
-};
+exports.mochaHooks = {};
