362242f9236dd10914b3e61e4fc51b23ca67a83f
Rinse <rinse12@tutanota.com>
2022-09-26 12:58:02 +0000

    set native functions is done in each test file now

--- test/browser/root-hook-karma.util.js
+++ test/browser/root-hook-karma.util.js
@@ -1,17 +1,3 @@
-import Plebbit from "../../dist/browser";
-
-const setupNativeFunction = () => {
-    const isElectron = navigator?.userAgent?.includes("Electron");
-    if (isElectron) {
-        // If in electron env, set native functiosn
-        console.log(`Detected Electron env. Will set native functions`);
-        console.log(window.plebbitJsNativeFunctions);
-        Plebbit.setNativeFunctions(window.plebbitJsNativeFunctions);
-    } else {
-        console.log(`Detected non native Electron env`);
-    }
-};
-
 (function () {
-    setupNativeFunction();
+    // No need to set up anything here for now
 })();
