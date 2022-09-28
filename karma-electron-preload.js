2a88ef890fee5d897aab0b1f9c55ea798409d8c3
Rinse <rinse12@tutanota.com>
2022-09-28 05:52:03 +0000

    updated karma electrn configs to use Plebbit.nativeFunctions.node

--- karma-electron-preload.js
+++ karma-electron-preload.js
@@ -2,6 +2,7 @@
 // expose plebbit-js native functions into electron's renderer
 const {contextBridge} = require('electron')
 const path = require ('path')
-contextBridge.exposeInMainWorld('plebbitJsNativeFunctions', require(path.join(process.cwd(), 'dist/node/runtime/node/native-functions')).default)
+const Plebbit = require(__dirname)
+contextBridge.exposeInMainWorld('plebbitJsNativeFunctions', Plebbit.nativeFunctions.node)
 contextBridge.exposeInMainWorld('plebbitDataPath', path.join(process.cwd(), '.plebbit'))
 console.log('electron preload.js contextBridge.exposeInMainWorld plebbitJsNativeFunctions')
