87b4dd09ec8e22c36945862373e5d7a2562e7755
Rinse <rinse12@tutanota.com>
2022-10-26 06:47:21 +0000

    add mock ipfs gatewy to electron

--- karma-electron-preload.js
+++ karma-electron-preload.js
@@ -5,10 +5,4 @@ const path = require ('path')
 const Plebbit = require(__dirname)
 contextBridge.exposeInMainWorld('plebbitJsNativeFunctions', Plebbit.nativeFunctions.node)
 contextBridge.exposeInMainWorld('plebbitDataPath', path.join(process.cwd(), '.plebbit'))
-contextBridge.exposeInMainWorld('startMaliciousGateway', () => require("http").createServer((req, res) => {
-    if (req.url === "/ipfs/QmbWqTYuyfcpDyn6gawRf5eSFVtYnGDAKttjESXjjbAHbr") res.end("Hello plebs"); // Valid content
-    else if (req.url === "/ipfs/QmUFu8fzuT1th3jJYgR4oRgGpw3sgRALr4nbenA4pyoCav")
-        res.end("Invalid content. This should throw an error in plebbit.fetchCid");
-    else res.end("Unknown CID");
-}).listen(33415))
 console.log('electron preload.js contextBridge.exposeInMainWorld plebbitJsNativeFunctions')
