26b23995505664bd14acc569f3a87677108c5771
Rinse <rinse12@tutanota.com>
2022-12-04 11:09:34 +0000

    Added a new test file for testing DB and in particular its migration. Added a test for migrating from schema version 2 to 3

---
+++ test/node/db.subplebbit.test.js
@@ -0,0 +1,66 @@
+const Plebbit = require("../../dist/node");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const { expect, assert } = chai;
+
+const { mockPlebbit } = require("../../dist/node/test/test-util");
+const path = require("path");
+const fs = require("fs");
+const { generateMockPost, generateMockComment } = require("../../dist/node/test/test-util");
+
+const pathToDbFixtureVersionTwo = "test/fixtures/subplebbit_db_version_2/QmbdJpNU6cAgSXHjUNnSBrUZGBtStKPkdwKyiffqRy1x6c";
+
+if (globalThis["navigator"]?.userAgent?.includes("Electron")) Plebbit.setNativeFunctions(window.plebbitJsNativeFunctions);
+
+const syncInterval = 300;
+
+describe(`DB importing`, async () => {
+    let plebbit;
+
+    before(async () => {
+        plebbit = await mockPlebbit(globalThis["window"]?.plebbitDataPath);
+    });
+
+    it(`Subplebbit will show up in listSubplebbits if its db was copied to datapath/subplebbits`, async () => {
+        const subAddress = path.basename(pathToDbFixtureVersionTwo);
+        const newPath = path.join(plebbit.dataPath, "subplebbits", subAddress);
+        fs.cpSync(pathToDbFixtureVersionTwo, newPath);
+        const listedSubs = await plebbit.listSubplebbits();
+        expect(listedSubs).to.include(subAddress);
+    });
+});
+
+describe("DB Migration", () => {
+    let plebbit;
+
+    before(async () => {
+        plebbit = await mockPlebbit(globalThis["window"]?.plebbitDataPath);
+    });
+
+    it(`Can migrate from DB version 2 to 3`, async () => {
+        const subAddress = path.basename(pathToDbFixtureVersionTwo); // This sub has a DB version 2
+        // Once we start the sub, it's gonna attempt to migrate to the latest DB version (3 currently)
+
+        const subplebbit = await plebbit.createSubplebbit({ address: subAddress });
+
+        const currentDbVersion = await subplebbit.dbHandler.getDbVersion();
+        expect(currentDbVersion).to.equal(3); // If they're equal, that means all tables have been migrated
+
+        subplebbit.setProvideCaptchaCallback(async () => [[], "Challenge skipped"]);
+
+        subplebbit._syncIntervalMs = syncInterval;
+        assert.isFulfilled(subplebbit.start());
+
+        const commentToPostUnder = await plebbit.createComment(subplebbit.posts.pages.hot.comments[0]);
+        commentToPostUnder._updateIntervalMs = syncInterval;
+
+        const mockComment = await generateMockComment(commentToPostUnder, plebbit);
+        await mockComment.publish();
+
+        await new Promise((resolve) => {
+            commentToPostUnder.update();
+            commentToPostUnder.once("update", resolve);
+        });
+    });
+});
