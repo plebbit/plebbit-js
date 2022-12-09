a7a7c96b9fd2869f7fb7de4d10213a9ae9eecec8
Rinse <rinse12@tutanota.com>
2022-12-09 12:43:01 +0000

    signer.ipfsKey is disabled by default

--- test/node-and-browser/plebbit.test.js
+++ test/node-and-browser/plebbit.test.js
@@ -61,7 +61,6 @@ describe("plebbit (node and browser)", async () => {
             expect(signer.publicKey).to.match(/-----BEGIN PUBLIC KEY-----/);
             expect(signer.address).to.match(/^Qm/);
             expect(signer.type).to.equal("rsa");
-            expect(signer.ipfsKey?.constructor.name).to.equal("Uint8Array");
         });
 
         it("with private key argument", async () => {
@@ -71,7 +70,6 @@ describe("plebbit (node and browser)", async () => {
             expect(signer.publicKey).to.equal(fixtureSigner.publicKey);
             expect(signer.address).to.equal(fixtureSigner.address);
             expect(signer.type).to.equal("rsa");
-            expect(signer.ipfsKey?.constructor.name).to.equal("Uint8Array");
         });
 
         it("generate same signer twice", async () => {
