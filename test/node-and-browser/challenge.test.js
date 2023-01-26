5e48fecb0989a55ee7a0f74c90c3dcb74595c6c4
Rinse <rinse12@tutanota.com>
2023-01-26 17:10:00 +0000

    clean tests

--- test/node-and-browser/challenge.test.js
+++ test/node-and-browser/challenge.test.js
@@ -1,8 +1,9 @@
 const Plebbit = require("../../dist/node");
 const { expect } = require("chai");
 const signers = require("../fixtures/signers");
-const { generateMockPost } = require("../../dist/node/test/test-util");
+const { generateMockPost, publishWithExpectedResult } = require("../../dist/node/test/test-util");
 const { mockPlebbit } = require("../../dist/node/test/test-util");
+const lodash = require("lodash");
 
 if (globalThis["navigator"]?.userAgent?.includes("Electron")) Plebbit.setNativeFunctions(window.plebbitJsNativeFunctions);
 
@@ -16,32 +17,20 @@ describe("math-cli", async () => {
         plebbit = await mockPlebbit();
     });
     it("can post after answering correctly", async function () {
-        return new Promise(async (resolve, reject) => {
             const mockPost = await generateMockPost(mathCliSubplebbitAddress, plebbit, signers[0]);
             mockPost.removeAllListeners();
-            await mockPost.publish();
             mockPost.once("challenge", (challengeMessage) => {
                 mockPost.publishChallengeAnswers(["2"]);
             });
-            mockPost.once("challengeverification", async (challengeVerificationMessage, newComment) => {
-                expect(challengeVerificationMessage.challengeSuccess).to.be.true;
-                resolve();
-            });
-        });
+        await publishWithExpectedResult(mockPost, true);
     });
     it("Throws an error when user fails to solve mathcli captcha", async function () {
-        return new Promise(async (resolve, reject) => {
             const mockPost = await generateMockPost(mathCliSubplebbitAddress, plebbit, signers[0]);
             mockPost.removeAllListeners();
             mockPost.once("challenge", (challengeMessage) => {
                 mockPost.publishChallengeAnswers(["3"]);
             });
-            await mockPost.publish();
-            mockPost.once("challengeverification", (challengeVerificationMessage, newComment) => {
-                expect(challengeVerificationMessage.challengeSuccess).to.be.false;
-                resolve();
-            });
-        });
+        await publishWithExpectedResult(mockPost, false);
     });
 });
 
@@ -51,7 +40,6 @@ describe("image captcha", async () => {
         plebbit = await mockPlebbit();
     });
     it("can post after answering correctly", async function () {
-        return new Promise(async (resolve, reject) => {
             const mockPost = await generateMockPost(imageCaptchaSubplebbitAddress, plebbit, signers[0]);
             mockPost.removeAllListeners();
 
@@ -60,12 +48,12 @@ describe("image captcha", async () => {
                 await mockPost.publishChallengeAnswers(["1234"]); // hardcode answer here
             });
 
-            await mockPost.publish();
-            mockPost.once("challengeverification", (challengeVerificationMessage, newComment) => {
-                expect(challengeVerificationMessage.challengeSuccess).to.be.true;
-                expect(challengeVerificationMessage.reason).to.be.undefined;
-                resolve();
+        await publishWithExpectedResult(mockPost, true);
             });
+
+    it("Throws an error if unable to solve image captcha", async function () {
+        const mockPost = await generateMockPost(imageCaptchaSubplebbitAddress, plebbit, signers[0]);
+        await publishWithExpectedResult(mockPost, false);
         });
     });
 
