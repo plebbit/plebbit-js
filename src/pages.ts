e304d4f55c0ec05d49752c09443b60045095dcfc
Rinse <rinse12@tutanota.com>
2023-02-17 08:22:56 +0000

    substitute Page for PageType

--- src/pages.ts
+++ src/pages.ts
@@ -2,7 +2,6 @@ import { loadIpfsFileAsJson, parsePageIpfs, throwWithErrorCode } from "./util";
 import { Subplebbit } from "./subplebbit";
 
 import { CommentIpfsType, PageIpfs, PagesType, PageType, PostSortName, ReplySortName } from "./types";
-import { Comment } from "./comment";
 import isIPFS from "is-ipfs";
 import { verifyPage } from "./signer/signatures";
 
@@ -18,15 +17,14 @@ export class Pages implements PagesType {
         this._parentCid = props.parentCid;
     }
 
-    async getPage(pageCid: string): Promise<Page> {
+    async getPage(pageCid: string): Promise<PageType> {
         if (!isIPFS.cid(pageCid)) throwWithErrorCode("ERR_CID_IS_INVALID", `getPage: cid (${pageCid}) is invalid as a CID`);
 
         const pageIpfs: PageIpfs = await loadIpfsFileAsJson(pageCid, this._subplebbit.plebbit);
         const signatureValidity = await verifyPage(pageIpfs, this._subplebbit.plebbit, this._subplebbit, this._parentCid);
         if (!signatureValidity.valid) throw Error(signatureValidity.reason);
 
-        const parsedPage = await parsePageIpfs(pageIpfs, this._subplebbit.plebbit);
-        return new Page(parsedPage);
+        return await parsePageIpfs(pageIpfs, this._subplebbit);
     }
 
     toJSON(): PagesType {
@@ -34,19 +32,5 @@ export class Pages implements PagesType {
             pages: this.pages,
             pageCids: this.pageCids
         };
-    }
-}
-
-export class Page implements PageType {
-    comments: Comment[];
-    nextCid?: string;
-
-    constructor(props: PageType) {
-        this.comments = props.comments;
-        this.nextCid = props.nextCid;
-    }
-
-    toJSON(): PageType {
-        return { comments: this.comments, nextCid: this.nextCid };
     }
 }
