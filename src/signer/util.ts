4bb60dd88799cfeaca48c38f63746ec5875d7172
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-03-26 06:10:48 +0000

    fix bug

--- src/signer/util.ts
+++ src/signer/util.ts
@@ -1,7 +1,7 @@
 // NOTE: Ed25519PublicKey, Ed25519PrivateKey are not public apis, could break when upgrading libp2p-crypto
 import {Ed25519PublicKey, Ed25519PrivateKey} from 'libp2p-crypto/src/keys/ed25519-class'
 import PeerId from "peer-id";
-import ed from '@noble/ed25519'
+import * as ed from '@noble/ed25519'
 import {fromString as uint8ArrayFromString} from 'uint8arrays/from-string'
 import {toString as uint8ArrayToString} from 'uint8arrays/to-string'
 
