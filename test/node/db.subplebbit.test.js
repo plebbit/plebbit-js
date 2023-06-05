6bf5c6b3a9b033f8d1effe73bd19f5cf3129d1b3
Rinse <rinse12@tutanota.com>
2023-06-05 20:30:34 +0000

    add test for upgrading from db version 6 to 7

--- test/node/db.subplebbit.test.js
+++ test/node/db.subplebbit.test.js
@@ -11,7 +11,10 @@ const { mockPlebbit, publishRandomReply, publishRandomPost } = require("../../di
 
 const plebbitVersion = require("../../dist/node/version");
 
-const databases = [{ version: 5, address: "12D3KooWN5rLmRJ8fWMwTtkDN7w2RgPPGRM4mtWTnfbjpi1Sh7zR" }];
+const databases = [
+    { version: 5, address: "12D3KooWN5rLmRJ8fWMwTtkDN7w2RgPPGRM4mtWTnfbjpi1Sh7zR" },
+    { version: 6, address: "12D3KooWN5rLmRJ8fWMwTtkDN7w2RgPPGRM4mtWTnfbjpi1Sh7zR" }
+];
 if (globalThis["navigator"]?.userAgent?.includes("Electron")) Plebbit.setNativeFunctions(window.plebbitJsNativeFunctions);
 
 const plebbitOptions = {
@@ -20,43 +23,39 @@ const plebbitOptions = {
     pubsubHttpClientsOptions: ["http://localhost:15005/api/v0"]
 };
 console.log(`PlebbitOptions for db.subplebbit.test.js`, plebbitOptions);
+
+const copyDbToDataPath = async (databaseObj, plebbit) => {
+    const dbPath = path.join(process.cwd(), "test", "fixtures", "subplebbits_dbs", `version_${databaseObj.version}`, databaseObj.address);
+
+    const subAddress = path.basename(dbPath);
+    const newPath = path.join(plebbit.dataPath, "subplebbits", subAddress);
+    await fs.promises.cp(dbPath, newPath);
+};
 describe(`DB importing`, async () => {
     let plebbit;
-
-    const copyDbToDataPath = async (database) => {
-        const dbPath = path.join(process.cwd(), "test", "fixtures", "subplebbits_dbs", `version_${database.version}`, database.address);
-
-        const subAddress = path.basename(dbPath);
-        const newPath = path.join(plebbit.dataPath, "subplebbits", subAddress);
-        await fs.promises.cp(dbPath, newPath);
-    };
 
     before(async () => {
         plebbit = await mockPlebbit(plebbitOptions);
     });
 
-    after(async () => {
-        // Copy rest of databases
-        await Promise.all(databases.splice(1).map(copyDbToDataPath));
-    });
-
     it(`Subplebbit will show up in listSubplebbits if its db was copied to datapath/subplebbits`, async () => {
-        await copyDbToDataPath(databases[0]);
+        await copyDbToDataPath(databases[0], plebbit);
         const listedSubs = await plebbit.listSubplebbits();
         expect(listedSubs).to.include(databases[0].address);
     });
 });
 
 describe("DB Migration", () => {
-    let plebbit;
-
-    before(async () => {
-        plebbit = await mockPlebbit(plebbitOptions);
-    });
-
     databases.map((database) =>
         it(`Can migrate from DB version ${database.version} to ${plebbitVersion.default.DB_VERSION}`, async () => {
             // Once we start the sub, it's gonna attempt to migrate to the latest DB version
+
+            const plebbit = await mockPlebbit({ ...plebbitOptions, dataPath: tempy.directory() });
+
+            console.log(
+                `We're using datapath (${plebbit.dataPath}) For testing migration from db version (${database.version}) to ${plebbitVersion.default.DB_VERSION}`
+            );
+            await copyDbToDataPath(database, plebbit);
 
             const subplebbit = await plebbit.createSubplebbit({ address: database.address });
 
@@ -70,6 +69,7 @@ describe("DB Migration", () => {
             const post = await publishRandomPost(subplebbit.address, plebbit);
             await publishRandomReply(post, plebbit);
             await subplebbit.stop();
+            await subplebbit.delete();
         }).timeout(400000)
     );
 });
