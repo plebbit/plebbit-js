70904b9d3493653f446e99e9e308615a381b072d
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-09-14 19:38:53 +0000

    add netlify plebones build script

---
+++ scripts/build-plebones.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# go to current folder
+cd "$(dirname "$0")"
+cd ..
+
+# build and bundle
+# npm install
+# npm run build
+
+# build plebones
+# git clone https://github.com/plebbit/plebones.git plebones
+cd plebones
+# yarn
+
+# replace old plebbit-js in plebones deps with latest plebbit-js
+rm -fr node_modules/@plebbit/plebbit-js/dist
+cp -r ../dist node_modules/@plebbit/plebbit-js/dist
+
+# build plebones
+yarn build
+
+# copy plebones build to plebbit-js build folder
+cp -r build ../build
