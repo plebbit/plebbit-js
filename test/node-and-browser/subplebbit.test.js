c5f77ebabb9fe23d6a3e249f9131e0037b1d8f9e
Rinse <rinse12@tutanota.com>
2023-10-05 13:34:18 +0000

    add tests for subplebbit.posts.clients.plebbitRpcClients

--- test/node-and-browser/subplebbit.test.js
+++ test/node-and-browser/subplebbit.test.js
@@ -420,5 +420,31 @@ describe(`subplebbit.clients (Remote)`, async () => {
                 expect(actualStates).to.deep.equal(expectedStates);
             });
         });
+
+        //prettier-ignore
+        if (process.env["USE_RPC"] === "1")
+        describe.only(`subplebbit.posts.clients.plebbitRpcClients`, async () => {
+            it(`subplebbit.posts.clients.ipfsGateways[sortType][url] is stopped by default`, async () => {
+                const mockSub = await plebbit.getSubplebbit(subplebbitAddress);
+                const rpcUrl = Object.keys(mockSub.clients.plebbitRpcClients)[0];
+                // add tests here
+                expect(Object.keys(mockSub.posts.clients.plebbitRpcClients["new"]).length).to.equal(1);
+                expect(mockSub.posts.clients.plebbitRpcClients["new"][rpcUrl].state).to.equal("stopped");
+            });
+
+            it(`Correct state of 'new' sort is updated after fetching from subplebbit.posts.pageCids.new`, async () => {
+                const mockSub = await plebbit.getSubplebbit(subplebbitAddress);
+                const rpcUrl = Object.keys(mockSub.clients.plebbitRpcClients)[0];
+
+                const expectedStates = ["fetching-ipfs", "stopped"];
+                const actualStates = [];
+                mockSub.posts.clients.plebbitRpcClients["new"][rpcUrl].on("statechange", (newState) => {
+                    actualStates.push(newState);
+                });
+
+                await mockSub.posts.getPage(mockSub.posts.pageCids.new);
+                expect(actualStates).to.deep.equal(expectedStates);
+            });
+        })
     });
 });
