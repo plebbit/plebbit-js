b2adaa1933430cc6abcfc70bba2bdf2fda58e2e9
Rinse <rinse12@tutanota.com>
2023-11-05 13:40:36 +0000

    comment it out till we figure

--- test/root-hook-mocha.js
+++ test/root-hook-mocha.js
@@ -3,6 +3,6 @@ const { destroyMockIpfsClient } = require("../dist/node/test/mock-ipfs-client");
 exports.mochaHooks = {
     afterAll: () => {
         console.log("Test running afterAll");
-        destroyMockIpfsClient();
+        // destroyMockIpfsClient(); // Need to figure out how to destory io client with parallel mocha
     }
 };
