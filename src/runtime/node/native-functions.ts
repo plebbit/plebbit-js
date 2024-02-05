318a99965b4591a682c76da404f79d2325a285c4
Rinse <rinse12@tutanota.com>
2024-02-05 09:01:04 +0000

    fix bug with node fetch failing to resolve localhost

--- src/runtime/node/native-functions.ts
+++ src/runtime/node/native-functions.ts
@@ -1,5 +1,6 @@
 import { NativeFunctions } from "../../types";
-
+import dns from "node:dns";
+dns.setDefaultResultOrder("ipv4first");
 const nativeFunctions: NativeFunctions = {
     //@ts-expect-error
     fetch: async (...args) => fetch(...args)
