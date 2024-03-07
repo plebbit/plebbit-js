a9611fc376a395068023b6edbd03506d66da6b37
Rinse <rinse12@tutanota.com>
2024-03-07 08:25:41 +0000

    export static function like getShortCid, getShortAddress, etc

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import polyfill from "./runtime/node/polyfill.js";
 polyfill();
 import * as PlebbitClass from "./plebbit.js";
 import { PlebbitOptions } from "./types.js";
-import { setNativeFunctions } from "./runtime/node/util.js";
+import { setNativeFunctions as utilSetNativeFunctions } from "./runtime/node/util.js";
 import nodeNativeFunctions from "./runtime/node/native-functions.js";
 import browserNativeFunctions from "./runtime/browser/native-functions.js";
 import { shortifyAddress, shortifyCid } from "./util.js";
@@ -14,9 +14,14 @@ const Plebbit = async function Plebbit(plebbitOptions: PlebbitOptions = {}): Pro
     return plebbit;
 };
 
-Plebbit.setNativeFunctions = setNativeFunctions;
+Plebbit.setNativeFunctions = utilSetNativeFunctions;
 Plebbit.nativeFunctions = { node: nodeNativeFunctions, browser: browserNativeFunctions };
 Plebbit.getShortCid = shortifyCid;
 Plebbit.getShortAddress = shortifyAddress;
 Plebbit.challenges = plebbitJsChallenges;
 export default Plebbit;
+export const setNativeFunctions = Plebbit.setNativeFunctions;
+export const nativeFunctions = Plebbit.nativeFunctions;
+export const getShortCid = Plebbit.getShortCid;
+export const getShortAddress = Plebbit.getShortAddress;
+export const challenges = Plebbit.challenges;
