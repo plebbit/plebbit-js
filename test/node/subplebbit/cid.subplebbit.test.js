ef53a2b4b44ab11edfac766f927a6ede6d467262
Rinse <rinse12@tutanota.com>
2024-07-26 11:04:39 +0000

    add tests for subplebbit.cid

---
+++ test/node/subplebbit/cid.subplebbit.test.js
@@ -0,0 +1,35 @@
+import { mockPlebbit, resolveWhenConditionIsTrue } from "../../../dist/node/test/test-util";
+import chai from "chai";
+
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+const { expect, assert } = chai;
+describe(`Subplebbit.cid`, async () => {
+    let plebbit;
+    before(async () => {
+        plebbit = await mockPlebbit();
+    });
+    it(`subplebbit.cid gets updated when local-subplebbit publishes a new record`, async () => {
+        const sub = await plebbit.createSubplebbit({});
+        expect(sub.cid).to.be.undefined;
+
+        await sub.start();
+        await resolveWhenConditionIsTrue(sub, () => typeof sub.updatedAt === "number"); // wait until we publish a new record
+        expect(sub.cid).to.be.a("string");
+
+        await sub.delete();
+    });
+    it(`subplebbit.cid is defined when creating an instance of an existing local subplebbit`, async () => {
+        const sub = await plebbit.createSubplebbit({});
+        expect(sub.cid).to.be.undefined;
+
+        await sub.start();
+        await resolveWhenConditionIsTrue(sub, () => typeof sub.updatedAt === "number"); // wait until we publish a new record
+        expect(sub.cid).to.be.a("string");
+
+        const recreatedSub = await plebbit.createSubplebbit({ address: sub.address });
+        expect(recreatedSub.cid).to.equal(sub.cid);
+
+        await sub.delete();
+    });
+});
