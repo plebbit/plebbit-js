a7e69460823b541af32eac1d6d1d52dd6eaa6115
Rinse <rinse12@tutanota.com>
2024-08-07 12:50:11 +0000

    add test for publishing vote with reserved field

--- test/node-and-browser/publications/vote/backward.compatibility.vote.test.js
+++ test/node-and-browser/publications/vote/backward.compatibility.vote.test.js
@@ -6,6 +6,8 @@ import {
     publishRandomPost,
     publishWithExpectedResult
 } from "../../../../dist/node/test/test-util";
+
+import { messages } from "../../../../dist/node/errors.js";
 import signers from "../../../fixtures/signers.js";
 
 getRemotePlebbitConfigs().map((config) => {
@@ -27,6 +29,15 @@ getRemotePlebbitConfigs().map((config) => {
             disableZodValidationOfPublication(vote);
 
             await publishWithExpectedResult(vote, true);
+        });
+
+        it(`Publishing vote.reservedField should be rejected`, async () => {
+            const vote = await generateMockVote(commentToVoteOn, 1, plebbit);
+            const orgPublication = vote.toJSONPubsubMessagePublication();
+            vote.toJSONPubsubMessagePublication = () => ({ ...orgPublication, insertedAt: "1234" });
+            disableZodValidationOfPublication(vote);
+
+            await publishWithExpectedResult(vote, false, messages.ERR_VOTE_HAS_RESERVED_FIELD);
         });
     });
 });
