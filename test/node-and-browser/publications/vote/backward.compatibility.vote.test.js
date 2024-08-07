b53120daa109b646623ddcc046bd991b31577b85
Rinse <rinse12@tutanota.com>
2024-08-07 15:32:51 +0000

    forgot to use config.plebbitInstance

--- test/node-and-browser/publications/vote/backward.compatibility.vote.test.js
+++ test/node-and-browser/publications/vote/backward.compatibility.vote.test.js
@@ -18,20 +18,20 @@ getRemotePlebbitConfigs().map((config) => {
         let plebbit;
         let commentToVoteOn;
         before(async () => {
-            plebbit = await mockPlebbit();
+            plebbit = await config.plebbitInstancePromise();
             commentToVoteOn = await publishRandomPost(signers[0].address, plebbit, {}, false);
         });
 
         it(`Publishing vote.extraProp should fail if it's not included in vote.signature.signedPropertyNames`, async () => {
             const vote = await generateMockVote(commentToVoteOn, 1, plebbit);
-            await setExtraPropOnVoteAndSign(vote, { extraProp: "1234" }, false);
+            await setExtraPropOnVoteAndSign(vote, { extraProp: "1234" }, false); // will include extra prop in request.publication, but not in signedPropertyNames
 
             await publishWithExpectedResult(vote, false, messages.ERR_VOTE_RECORD_INCLUDES_FIELD_NOT_IN_SIGNED_PROPERTY_NAMES);
         });
 
         it(`publishing vote.extraProp should succeed if it's included in vote.signature.signedPropertyNames`, async () => {
             const vote = await generateMockVote(commentToVoteOn, 1, plebbit);
-            await setExtraPropOnVoteAndSign(vote, { extraProp: "1234" }, true);
+            await setExtraPropOnVoteAndSign(vote, { extraProp: "1234" }, true); // will include extra prop in request.publication, and signedPropertyNames
 
             await publishWithExpectedResult(vote, true);
         });
