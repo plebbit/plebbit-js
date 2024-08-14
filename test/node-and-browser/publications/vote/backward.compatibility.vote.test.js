1acf71c7adc403e081f738a10dd9a37f5520fe2e
Rinse <rinse12@tutanota.com>
2024-08-14 11:26:08 +0000

    make sure extra prop is included with challenge request

--- test/node-and-browser/publications/vote/backward.compatibility.vote.test.js
+++ test/node-and-browser/publications/vote/backward.compatibility.vote.test.js
@@ -1,11 +1,18 @@
 import {
     generateMockVote,
     getRemotePlebbitConfigs,
+    itSkipIfRpc,
     mockPlebbit,
     publishRandomPost,
     publishWithExpectedResult,
     setExtraPropOnVoteAndSign
 } from "../../../../dist/node/test/test-util";
+
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
+const { expect, assert } = chai;
 
 import { messages } from "../../../../dist/node/errors.js";
 import signers from "../../../fixtures/signers.js";
@@ -22,11 +29,14 @@ getRemotePlebbitConfigs().map((config) => {
             commentToVoteOn = await publishRandomPost(signers[0].address, plebbit, {}, false);
         });
 
-        it(`Publishing vote.extraProp should fail if it's not included in vote.signature.signedPropertyNames`, async () => {
+        itSkipIfRpc(`Publishing vote.extraProp should fail if it's not included in vote.signature.signedPropertyNames`, async () => {
+            // We skip with RPC because rpc server will check if signature is valid before publishing
+            // If signature is invalid, like in this test, it will throw before publishing
             const vote = await generateMockVote(commentToVoteOn, 1, plebbit);
             await setExtraPropOnVoteAndSign(vote, { extraProp: "1234" }, false); // will include extra prop in request.publication, but not in signedPropertyNames
 
             await publishWithExpectedResult(vote, false, messages.ERR_VOTE_RECORD_INCLUDES_FIELD_NOT_IN_SIGNED_PROPERTY_NAMES);
+            expect(vote._publishedChallengeRequests[0].publication.extraProp).to.equal("1234");
         });
 
         it(`publishing vote.extraProp should succeed if it's included in vote.signature.signedPropertyNames`, async () => {
@@ -34,6 +44,7 @@ getRemotePlebbitConfigs().map((config) => {
             await setExtraPropOnVoteAndSign(vote, { extraProp: "1234" }, true); // will include extra prop in request.publication, and signedPropertyNames
 
             await publishWithExpectedResult(vote, true);
+            expect(vote._publishedChallengeRequests[0].publication.extraProp).to.equal("1234");
         });
 
         it(`Publishing vote.reservedField should be rejected`, async () => {
@@ -41,6 +52,7 @@ getRemotePlebbitConfigs().map((config) => {
             await setExtraPropOnVoteAndSign(vote, { insertedAt: "1234" }, true);
 
             await publishWithExpectedResult(vote, false, messages.ERR_VOTE_HAS_RESERVED_FIELD);
+            expect(vote._publishedChallengeRequests[0].publication.insertedAt).to.equal("1234");
         });
     });
 });
