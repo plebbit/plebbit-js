754628a3054178f930be4960435de44ebc94a3af
Rinse <rinse12@tutanota.com>
2024-08-14 09:24:03 +0000

    added extra tests for sub transmitting updates needlessly

--- test/node/subplebbit/update.subplebbit.test.js
+++ test/node/subplebbit/update.subplebbit.test.js
@@ -1,4 +1,10 @@
-import { createSubWithNoChallenge, mockPlebbit, publishRandomPost, resolveWhenConditionIsTrue } from "../../../dist/node/test/test-util";
+import {
+    createSubWithNoChallenge,
+    isRpcFlagOn,
+    mockPlebbit,
+    publishRandomPost,
+    resolveWhenConditionIsTrue
+} from "../../../dist/node/test/test-util";
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
@@ -24,20 +30,24 @@ describe(`subplebbit.update - Local subs`, async () => {
         await sub.delete();
     });
 
-    it(`A local subplebbit is not emitting updates unneccessarily`, async () => {
+    it(`A local subplebbit is not emitting updates unneccessarily (after first update)`, async () => {
         const sub = await createSubWithNoChallenge({}, plebbit);
         await sub.start();
         await resolveWhenConditionIsTrue(sub, () => typeof sub.updatedAt === "number");
 
         const recreatedSub = await plebbit.createSubplebbit({ address: sub.address });
         const oldUpdatedAt = JSON.parse(JSON.stringify(recreatedSub.updatedAt));
+        expect(oldUpdatedAt).to.be.a("number");
         expect(oldUpdatedAt).to.equal(sub.updatedAt);
 
         await recreatedSub.update();
+        let updatesEmitted = 0;
 
         const failPromise = new Promise((resolve, reject) =>
             recreatedSub.on("update", () => {
-                if (recreatedSub.updatedAt === oldUpdatedAt) reject("It should not emit an update if there's no new info");
+                updatesEmitted++;
+                if (recreatedSub.updatedAt === oldUpdatedAt && updatesEmitted > 1)
+                    reject(new Error("It should not emit an update if there's no new info"));
             })
         );
 
@@ -49,5 +59,21 @@ describe(`subplebbit.update - Local subs`, async () => {
             await sub.delete();
             await recreatedSub.stop();
         }
+    });
+
+    it(`A local subplebbit is not emitted updates unnecessarily (before first update)`, async () => {
+        const sub = await createSubWithNoChallenge({}, plebbit);
+
+        await sub.update();
+
+        let emittedUpdates = 0;
+        sub.on("update", () => emittedUpdates++);
+
+        await new Promise((resolve) => setTimeout(resolve, plebbit.publishInterval * 3));
+
+        if (isRpcFlagOn()) expect(emittedUpdates).to.equal(1);
+        else expect(emittedUpdates).to.equal(0);
+
+        await sub.delete();
     });
 });
