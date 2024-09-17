85290dcd423604535901b7d61c5f0a0637cee814
Rinse <rinse12@tutanota.com>
2024-09-17 13:48:50 +0000

    fix commentEdit test errors

--- test/node-and-browser/publications/comment-edit/backward.compatibility.commentedit.test.js
+++ test/node-and-browser/publications/comment-edit/backward.compatibility.commentedit.test.js
@@ -1,7 +1,6 @@
 import {
     getRemotePlebbitConfigs,
     itSkipIfRpc,
-    mockPlebbit,
     publishRandomPost,
     publishWithExpectedResult,
     resolveWhenConditionIsTrue,
@@ -53,7 +52,7 @@ getRemotePlebbitConfigs().map((config) => {
                     false,
                     messages.ERR_COMMENT_EDIT_RECORD_INCLUDES_FIELD_NOT_IN_SIGNED_PROPERTY_NAMES
                 );
-                expect(commentEdit._publishedChallengeRequests[0].publication.extraProp).to.equal("1234");
+                expect(commentEdit._publishedChallengeRequests[0].commentEdit.extraProp).to.equal("1234");
             }
         );
 
@@ -72,26 +71,7 @@ getRemotePlebbitConfigs().map((config) => {
             // if commentToEdit emits update that means the signature of update.edit is correct
             expect(commentToEdit.content).to.equal(commentEdit.content); // should process only content since it's the known field
             expect(commentToEdit.extraProp).to.be.undefined;
-            expect(commentEdit._publishedChallengeRequests[0].publication.extraProp).to.equal("1234");
-            await plebbit.createCommentEdit(JSON.parse(JSON.stringify(commentEdit))); // Just to test if create will throw because of extra prop
-        });
-
-        it(`publishing commentEdit.extraProp should succeed as a mod edit if it's included in commentEdit.signature.signedPropertyNames`, async () => {
-            const commentEdit = await plebbit.createCommentEdit({
-                commentCid: commentToEdit.cid,
-                subplebbitAddress: commentToEdit.subplebbitAddress,
-                removed: true,
-                signer: signers[3]
-            });
-            await setExtraPropOnCommentEditAndSign(commentEdit, { extraProp: "1234" }, true);
-
-            await publishWithExpectedResult(commentEdit, true);
-
-            await new Promise((resolve) => commentToEdit.once("update", resolve));
-            // if commentToEdit emits update that means the signature of update.edit is correct
-            expect(commentToEdit.removed).to.be.true; // should process only removed since it's the known field
-            expect(commentToEdit.extraProp).to.be.undefined;
-            expect(commentEdit._publishedChallengeRequests[0].publication.extraProp).to.equal("1234");
+            expect(commentEdit._publishedChallengeRequests[0].commentEdit.extraProp).to.equal("1234");
             await plebbit.createCommentEdit(JSON.parse(JSON.stringify(commentEdit))); // Just to test if create will throw because of extra prop
         });
 
@@ -105,7 +85,7 @@ getRemotePlebbitConfigs().map((config) => {
             await setExtraPropOnCommentEditAndSign(commentEdit, { insertedAt: "1234" }, true);
 
             await publishWithExpectedResult(commentEdit, false, messages.ERR_COMMENT_EDIT_HAS_RESERVED_FIELD);
-            expect(commentEdit._publishedChallengeRequests[0].publication.insertedAt).to.equal("1234");
+            expect(commentEdit._publishedChallengeRequests[0].commentEdit.insertedAt).to.equal("1234");
         });
 
         describe(`Publishing CommentEdit with extra props in author field - ${config.name}`, async () => {
@@ -113,7 +93,7 @@ getRemotePlebbitConfigs().map((config) => {
                 const commentEdit = await plebbit.createCommentEdit({
                     commentCid: commentToEdit.cid,
                     subplebbitAddress: commentToEdit.subplebbitAddress,
-                    removed: true,
+                    deleted: true,
                     signer: signers[3]
                 });
 
@@ -124,14 +104,14 @@ getRemotePlebbitConfigs().map((config) => {
                 );
 
                 await publishWithExpectedResult(commentEdit, false, messages.ERR_PUBLICATION_AUTHOR_HAS_RESERVED_FIELD);
-                expect(commentEdit._publishedChallengeRequests[0].publication.author.subplebbit).to.equal("random");
+                expect(commentEdit._publishedChallengeRequests[0].commentEdit.author.subplebbit).to.equal("random");
             });
             it(`Publishing with extra prop for author should succeed`, async () => {
                 const commentEdit = await plebbit.createCommentEdit({
                     commentCid: commentToEdit.cid,
                     subplebbitAddress: commentToEdit.subplebbitAddress,
-                    removed: true,
-                    signer: signers[3]
+                    spoiler: true,
+                    signer: commentToEdit.signer
                 });
                 const extraProps = { extraProp: "1234" };
                 await setExtraPropOnCommentEditAndSign(
@@ -143,7 +123,7 @@ getRemotePlebbitConfigs().map((config) => {
                 await plebbit.createCommentEdit(JSON.parse(JSON.stringify(commentEdit))); // Just to test if create will throw because of extra prop
 
                 await publishWithExpectedResult(commentEdit, true);
-                expect(commentEdit._publishedChallengeRequests[0].publication.author.extraProp).to.equal(extraProps.extraProp);
+                expect(commentEdit._publishedChallengeRequests[0].commentEdit.author.extraProp).to.equal(extraProps.extraProp);
             });
         });
     });
--- test/node-and-browser/publications/comment-edit/misc.edit.test.js
+++ test/node-and-browser/publications/comment-edit/misc.edit.test.js
@@ -1,11 +1,8 @@
 import signers from "../../../fixtures/signers.js";
-import { messages } from "../../../../dist/node/errors.js";
-import * as remeda from "remeda";
 import {
     mockRemotePlebbit,
     publishRandomPost,
     publishWithExpectedResult,
-    setExtraPropOnCommentEditAndSign,
     resolveWhenConditionIsTrue
 } from "../../../../dist/node/test/test-util.js";
 import chai from "chai";
@@ -92,81 +89,6 @@ describe(`Changing multiple fields simultaneously in one CommentEdit`, async () 
         plebbit = await mockRemotePlebbit();
     });
 
-    it(`A mod can't mix author and mod edit fields`, async () => {
-        // The signer is both an author and a mod
-        const modPost = await publishRandomPost(subplebbitAddress, plebbit, { signer: roles[2].signer }, false);
-        const fieldsToChange = {
-            removed: true,
-            deleted: true,
-            pinned: true,
-            locked: true,
-            spoiler: true,
-            content: "Test change multiple fields" + Date.now()
-        };
-
-        try {
-            await plebbit.createCommentEdit({
-                ...fieldsToChange,
-                commentCid: modPost.cid,
-                signer: roles[2].signer,
-                subplebbitAddress
-            });
-        } catch (e) {
-            expect(e.errors[0].code).to.equal("unrecognized_keys");
-            expect(e.errors[0].keys).to.deep.equal(["removed", "pinned", "locked"]);
-        }
-
-        // We're gonna test publishing over pubsub, the sub should reject it
-        const commentEdit = await plebbit.createCommentEdit({
-            ...remeda.omit(fieldsToChange, ["locked", "pinned", "removed"]),
-            commentCid: modPost.cid,
-            signer: roles[2].signer,
-            subplebbitAddress
-        });
-
-        await setExtraPropOnCommentEditAndSign(commentEdit, fieldsToChange, true);
-        expect(commentEdit.toJSONPubsubMessagePublication().removed).to.be.true;
-
-        await publishWithExpectedResult(commentEdit, false, messages.ERR_PUBLISHING_EDIT_WITH_BOTH_MOD_AND_AUTHOR_FIELDS);
-    });
-
-    it(`A mod publishing multiple mod edit fields`, async () => {
-        const modPost = await publishRandomPost(subplebbitAddress, plebbit, { signer: roles[2].signer }, false);
-        const fieldsToChange = {
-            removed: true,
-            pinned: true,
-            locked: true,
-            spoiler: true,
-            reason: "Testing as a mod" + Date.now()
-        };
-
-        const edit = await plebbit.createCommentEdit({
-            ...fieldsToChange,
-            commentCid: modPost.cid,
-            signer: roles[2].signer,
-            subplebbitAddress
-        });
-        await publishWithExpectedResult(edit, true);
-        await modPost.update();
-
-        await resolveWhenConditionIsTrue(modPost, () => modPost.removed);
-        await modPost.stop();
-        expect(modPost.locked).to.be.true;
-        expect(modPost._rawCommentUpdate.locked).to.be.true;
-
-        expect(modPost.pinned).to.be.true;
-        expect(modPost._rawCommentUpdate.pinned).to.be.true;
-
-        expect(modPost.removed).to.be.true;
-        expect(modPost._rawCommentUpdate.removed).to.be.true;
-
-        expect(modPost.removed).to.be.true;
-        expect(modPost._rawCommentUpdate.spoiler).to.be.true;
-
-        expect(modPost.reason).to.equal(fieldsToChange.reason);
-        expect(modPost._rawCommentUpdate.reason).to.equal(fieldsToChange.reason);
-    });
-
     it(`An author publishing multiple author edit fields`, async () => {
         const authorPost = await publishRandomPost(subplebbitAddress, plebbit, {}, false); // random signer
 
@@ -210,61 +132,8 @@ describe(`Changing multiple edit fields in separate edits`, async () => {
         plebbit = await mockRemotePlebbit();
     });
 
-    it(`As a mod`, async () => {
-        const modPost = await publishRandomPost(subplebbitAddress, plebbit, { signer: roles[2].signer }, false);
-        const fieldsToChange = {
-            removed: true,
-            reason: "Testing removing",
-            pinned: true,
-            locked: true,
-            spoiler: true
-        };
-
-        const edit1 = await plebbit.createCommentEdit({
-            ...fieldsToChange,
-            commentCid: modPost.cid,
-            signer: modPost.signer,
-            subplebbitAddress
-        });
-        await publishWithExpectedResult(edit1, true);
-
-        fieldsToChange.removed = false;
-        fieldsToChange.reason = "Testing unremoving" + Date.now();
-        fieldsToChange.locked = false;
-        const edit2 = await plebbit.createCommentEdit({
-            ...fieldsToChange,
-            commentCid: modPost.cid,
-            signer: modPost.signer,
-            subplebbitAddress
-        });
-
-        await publishWithExpectedResult(edit2, true);
-
-        await modPost.update();
-        await resolveWhenConditionIsTrue(modPost, () => modPost.removed === fieldsToChange.removed);
-
-        await modPost.stop();
-        expect(modPost.locked).to.be.false;
-
-        expect(modPost._rawCommentUpdate.edit).to.be.undefined;
-        expect(modPost._rawCommentUpdate.locked).to.be.false;
-
-        expect(modPost.pinned).to.be.true;
-        expect(modPost._rawCommentUpdate.pinned).to.be.true;
-
-        expect(modPost.removed).to.be.false;
-        expect(modPost._rawCommentUpdate.removed).to.be.false;
-
-        expect(modPost.reason).to.equal(fieldsToChange.reason);
-        expect(modPost._rawCommentUpdate.reason).equal(fieldsToChange.reason);
-
-        expect(modPost.spoiler).to.be.true;
-        expect(modPost._rawCommentUpdate.spoiler).to.be.true;
-    });
-
     it(`as an author`, async () => {
-        const fieldsToChange = {
-            deleted: true,
+        const firstEditProps = {
             spoiler: true,
             content: "Test new content as author" + Date.now(),
             reason: "Test as an author" + Date.now()
@@ -273,18 +142,21 @@ describe(`Changing multiple edit fields in separate edits`, async () => {
         const authorPost = await publishRandomPost(subplebbitAddress, plebbit, {}, false); // generate random signer
 
         const edit1 = await plebbit.createCommentEdit({
-            ...fieldsToChange,
+            ...firstEditProps,
             commentCid: authorPost.cid,
             signer: authorPost.signer,
             subplebbitAddress
         });
         await publishWithExpectedResult(edit1, true);
 
-        fieldsToChange.deleted = false;
-        fieldsToChange.reason = "Testing undeleting" + Date.now();
-        fieldsToChange.content = "Test new content" + Date.now();
+        const secondEditProps = {
+            ...firstEditProps,
+            deleted: true,
+            reason: "Testing undeleting" + Date.now(),
+            content: "Test new content" + Date.now()
+        };
         const edit2 = await plebbit.createCommentEdit({
-            ...fieldsToChange,
+            ...secondEditProps,
             commentCid: authorPost.cid,
             signer: authorPost.signer,
             subplebbitAddress
@@ -293,86 +165,23 @@ describe(`Changing multiple edit fields in separate edits`, async () => {
         await publishWithExpectedResult(edit2, true);
 
         await authorPost.update();
-        await resolveWhenConditionIsTrue(authorPost, () => authorPost.deleted === fieldsToChange.deleted);
+        await resolveWhenConditionIsTrue(authorPost, () => authorPost.deleted === secondEditProps.deleted);
 
         await authorPost.stop();
-        expect(authorPost.deleted).to.be.false;
-        expect(authorPost._rawCommentUpdate.edit.deleted).to.be.false;
-        expect(authorPost._rawCommentUpdate.deleted).to.be.undefined;
+        expect(authorPost.deleted).to.equal(secondEditProps.deleted);
+        expect(authorPost._rawCommentUpdate.edit.deleted).to.equal(secondEditProps.deleted);
 
-        expect(authorPost.edit.reason).to.equal(fieldsToChange.reason);
-        expect(authorPost._rawCommentUpdate.edit.reason).to.equal(fieldsToChange.reason);
+        expect(authorPost.edit.reason).to.equal(secondEditProps.reason);
+        expect(authorPost._rawCommentUpdate.edit.reason).to.equal(secondEditProps.reason);
 
-        expect(authorPost.reason).to.be.undefined;
+        expect(authorPost.reason).to.be.undefined; // reserved for mods
         expect(authorPost._rawCommentUpdate.reason).to.be.undefined;
 
-        expect(authorPost.content).to.equal(fieldsToChange.content);
-        expect(authorPost._rawCommentUpdate.edit.content).to.equal(fieldsToChange.content);
+        expect(authorPost.content).to.equal(secondEditProps.content);
+        expect(authorPost._rawCommentUpdate.edit.content).to.equal(secondEditProps.content);
 
-        expect(authorPost.spoiler).to.be.true;
-        expect(authorPost._rawCommentUpdate.edit.spoiler).to.be.true;
+        expect(authorPost.spoiler).to.equal(firstEditProps.spoiler);
+        expect(authorPost._rawCommentUpdate.edit.spoiler).to.equal(firstEditProps.spoiler);
         expect(authorPost._rawCommentUpdate.spoiler).to.be.undefined;
-    });
-
-    it(`Correct value of CommentUpdate after author edit, then mod edit`, async () => {
-        const authorFieldsToChange = {
-            spoiler: true,
-            content: "Test new content as author" + Date.now(),
-            reason: "Test as an author" + Date.now()
-        };
-
-        const authorPost = await publishRandomPost(subplebbitAddress, plebbit, {}, false); // generate random signer
-
-        const authorEdit = await plebbit.createCommentEdit({
-            ...authorFieldsToChange,
-            commentCid: authorPost.cid,
-            signer: authorPost.signer,
-            subplebbitAddress
-        });
-        await publishWithExpectedResult(authorEdit, true);
-
-        const modFieldsToChange = {
-            removed: true,
-            reason: "Test remove as mod",
-            spoiler: false,
-            pinned: true
-        };
-        const modEdit = await plebbit.createCommentEdit({
-            ...modFieldsToChange,
-            commentCid: authorPost.cid,
-            signer: roles[2].signer,
-            subplebbitAddress
-        });
-
-        await publishWithExpectedResult(modEdit, true);
-
-        await authorPost.update();
-
-        await resolveWhenConditionIsTrue(authorPost, () => authorPost.removed === modFieldsToChange.removed);
-
-        await authorPost.stop();
-
-        // check mod changes here
-        expect(authorPost.removed).to.equal(modFieldsToChange.removed);
-        expect(authorPost._rawCommentUpdate.removed).to.equal(modFieldsToChange.removed);
-        expect(authorPost.reason).to.equal(modFieldsToChange.reason);
-        expect(authorPost._rawCommentUpdate.reason).to.equal(modFieldsToChange.reason);
-
-        expect(authorPost.spoiler).to.equal(modFieldsToChange.spoiler);
-        expect(authorPost._rawCommentUpdate.spoiler).to.equal(modFieldsToChange.spoiler);
-        expect(authorPost.pinned).to.equal(modFieldsToChange.pinned);
-        expect(authorPost._rawCommentUpdate.pinned).to.equal(modFieldsToChange.pinned);
-
-        // Check author changes here
-
-        expect(authorPost._rawCommentUpdate.edit.spoiler).to.equal(authorFieldsToChange.spoiler);
-        expect(authorPost.edit.spoiler).to.equal(authorFieldsToChange.spoiler);
-
-        expect(authorPost.content).to.equal(authorFieldsToChange.content);
-        expect(authorPost.edit.content).to.equal(authorFieldsToChange.content);
-        expect(authorPost._rawCommentUpdate.edit.content).to.equal(authorFieldsToChange.content);
-
-        expect(authorPost.edit.reason).to.equal(authorFieldsToChange.reason);
-        expect(authorPost._rawCommentUpdate.edit.reason).to.equal(authorFieldsToChange.reason);
     });
 });
