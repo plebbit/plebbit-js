fcb87063c6fa60610de3b344169606f6b6821f36
Rinse <rinse12@tutanota.com>
2024-10-21 10:59:47 +0000

    implement dynamic remote config tests for vote

--- test/node-and-browser/publications/vote/downvote.test.js
+++ test/node-and-browser/publications/vote/downvote.test.js
@@ -4,10 +4,9 @@ import {
     publishRandomPost,
     publishRandomReply,
     publishWithExpectedResult,
-    mockRemotePlebbit,
-    resolveWhenConditionIsTrue
+    resolveWhenConditionIsTrue,
+    getRemotePlebbitConfigs
 } from "../../../../dist/node/test/test-util.js";
-import { messages } from "../../../../dist/node/errors.js";
 import * as remeda from "remeda";
 
 import chai from "chai";
@@ -17,110 +16,113 @@ chai.use(chaiAsPromised);
 
 const subplebbitAddress = signers[0].address;
 
-const previousVotes = [];
-describe(`Test Downvote`, async () => {
-    let plebbit, postToVote, replyToVote, signer;
-    before(async () => {
-        plebbit = await mockRemotePlebbit();
-        signer = await plebbit.createSigner();
-        postToVote = await publishRandomPost(subplebbitAddress, plebbit, { signer }, false);
-        replyToVote = await publishRandomReply(postToVote, plebbit, { signer }, false);
-        await Promise.all([postToVote.update(), replyToVote.update()]);
-        await resolveWhenConditionIsTrue(postToVote, () => typeof postToVote.updatedAt === "number");
-        await resolveWhenConditionIsTrue(replyToVote, () => typeof replyToVote.updatedAt === "number");
-    });
-    after(async () => {
-        await postToVote.stop();
-        await replyToVote.stop();
-    });
+getRemotePlebbitConfigs().map((config) => {
+    describe(`Test Downvote - ${config.name}`, async () => {
+        const previousVotes = [];
 
-    it("Can downvote a post", async () => {
-        const originalDownvote = remeda.clone(postToVote.downvoteCount);
-        const vote = await generateMockVote(postToVote, -1, plebbit);
-        await publishWithExpectedResult(vote, true);
-
-        await resolveWhenConditionIsTrue(postToVote, () => postToVote.downvoteCount === originalDownvote + 1);
-
-        expect(postToVote.downvoteCount).to.equal(originalDownvote + 1);
-        expect(postToVote.upvoteCount).to.equal(0);
-        expect(postToVote.author.subplebbit.replyScore).to.equal(0);
-        expect(postToVote.author.subplebbit.postScore).to.equal(-1);
-        expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-        previousVotes.push(vote);
-    });
-
-    it(`Can downvote a reply`, async () => {
-        const originalDownvote = remeda.clone(replyToVote.downvoteCount);
-        const vote = await generateMockVote(replyToVote, -1, plebbit);
-        await publishWithExpectedResult(vote, true);
-
-        await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.downvoteCount === originalDownvote + 1);
-
-        expect(replyToVote.downvoteCount).to.equal(originalDownvote + 1);
-        expect(replyToVote.upvoteCount).to.equal(0);
-        expect(replyToVote.author.subplebbit.replyScore).to.equal(-1);
-        expect(replyToVote.author.subplebbit.postScore).to.equal(-1);
-        expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-
-        previousVotes.push(vote);
-    });
-
-    it("Can change post downvote to upvote", async () => {
-        const originalUpvote = remeda.clone(postToVote.upvoteCount);
-        const originalDownvote = remeda.clone(postToVote.downvoteCount);
-        const vote = await plebbit.createVote({
-            commentCid: previousVotes[0].commentCid,
-            subplebbitAddress: previousVotes[0].subplebbitAddress,
-            signer: previousVotes[0].signer,
-            vote: 1
+        let plebbit, postToVote, replyToVote, signer;
+        before(async () => {
+            plebbit = await config.plebbitInstancePromise();
+            signer = await plebbit.createSigner();
+            postToVote = await publishRandomPost(subplebbitAddress, plebbit, { signer }, false);
+            replyToVote = await publishRandomReply(postToVote, plebbit, { signer }, false);
+            await Promise.all([postToVote.update(), replyToVote.update()]);
+            await resolveWhenConditionIsTrue(postToVote, () => typeof postToVote.updatedAt === "number");
+            await resolveWhenConditionIsTrue(replyToVote, () => typeof replyToVote.updatedAt === "number");
         });
-        await publishWithExpectedResult(vote, true);
-
-        await resolveWhenConditionIsTrue(postToVote, () => postToVote.upvoteCount === originalUpvote + 1);
-
-        expect(postToVote.upvoteCount).to.equal(originalUpvote + 1);
-        expect(postToVote.downvoteCount).to.equal(originalDownvote - 1);
-        expect(postToVote.author.subplebbit.postScore).to.equal(1);
-        expect(postToVote.author.subplebbit.replyScore).to.equal(-1);
-        expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-    });
-
-    it("Can change reply downvote to upvote", async () => {
-        const originalUpvote = remeda.clone(replyToVote.upvoteCount);
-        const originalDownvote = remeda.clone(replyToVote.downvoteCount);
-        const vote = await plebbit.createVote({
-            commentCid: previousVotes[1].commentCid,
-            subplebbitAddress: previousVotes[1].subplebbitAddress,
-            signer: previousVotes[1].signer,
-            vote: 1
+        after(async () => {
+            await postToVote.stop();
+            await replyToVote.stop();
         });
-        await publishWithExpectedResult(vote, true);
 
-        await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.upvoteCount === originalUpvote + 1);
+        it("Can downvote a post", async () => {
+            const originalDownvote = remeda.clone(postToVote.downvoteCount);
+            const vote = await generateMockVote(postToVote, -1, plebbit);
+            await publishWithExpectedResult(vote, true);
 
-        expect(replyToVote.upvoteCount).to.equal(originalUpvote + 1);
-        expect(replyToVote.downvoteCount).to.equal(originalDownvote - 1);
-        expect(replyToVote.author.subplebbit.postScore).to.equal(1);
-        expect(replyToVote.author.subplebbit.replyScore).to.equal(1);
-        expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-    });
+            await resolveWhenConditionIsTrue(postToVote, () => postToVote.downvoteCount === originalDownvote + 1);
 
-    it("plebbit.createVote fails when commentCid is invalid ", async () => {
-        try {
-            await plebbit.createVote({
-                vote: previousVotes[1].vote,
+            expect(postToVote.downvoteCount).to.equal(originalDownvote + 1);
+            expect(postToVote.upvoteCount).to.equal(0);
+            expect(postToVote.author.subplebbit.replyScore).to.equal(0);
+            expect(postToVote.author.subplebbit.postScore).to.equal(-1);
+            expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+            previousVotes.push(vote);
+        });
+
+        it(`Can downvote a reply`, async () => {
+            const originalDownvote = remeda.clone(replyToVote.downvoteCount);
+            const vote = await generateMockVote(replyToVote, -1, plebbit);
+            await publishWithExpectedResult(vote, true);
+
+            await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.downvoteCount === originalDownvote + 1);
+
+            expect(replyToVote.downvoteCount).to.equal(originalDownvote + 1);
+            expect(replyToVote.upvoteCount).to.equal(0);
+            expect(replyToVote.author.subplebbit.replyScore).to.equal(-1);
+            expect(replyToVote.author.subplebbit.postScore).to.equal(-1);
+            expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+
+            previousVotes.push(vote);
+        });
+
+        it("Can change post downvote to upvote", async () => {
+            const originalUpvote = remeda.clone(postToVote.upvoteCount);
+            const originalDownvote = remeda.clone(postToVote.downvoteCount);
+            const vote = await plebbit.createVote({
+                commentCid: previousVotes[0].commentCid,
+                subplebbitAddress: previousVotes[0].subplebbitAddress,
+                signer: previousVotes[0].signer,
+                vote: 1
+            });
+            await publishWithExpectedResult(vote, true);
+
+            await resolveWhenConditionIsTrue(postToVote, () => postToVote.upvoteCount === originalUpvote + 1);
+
+            expect(postToVote.upvoteCount).to.equal(originalUpvote + 1);
+            expect(postToVote.downvoteCount).to.equal(originalDownvote - 1);
+            expect(postToVote.author.subplebbit.postScore).to.equal(1);
+            expect(postToVote.author.subplebbit.replyScore).to.equal(-1);
+            expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+        });
+
+        it("Can change reply downvote to upvote", async () => {
+            const originalUpvote = remeda.clone(replyToVote.upvoteCount);
+            const originalDownvote = remeda.clone(replyToVote.downvoteCount);
+            const vote = await plebbit.createVote({
+                commentCid: previousVotes[1].commentCid,
                 subplebbitAddress: previousVotes[1].subplebbitAddress,
                 signer: previousVotes[1].signer,
-                commentCid: "gibbrish"
+                vote: 1
             });
-            expect.fail("should fail");
-        } catch (e) {
-            expect(e.code).to.equal("ERR_INVALID_CREATE_VOTE_ARGS_SCHEMA");
-            expect(e.details.zodError.issues[0].message).to.equal("CID is invalid");
-        }
+            await publishWithExpectedResult(vote, true);
+
+            await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.upvoteCount === originalUpvote + 1);
+
+            expect(replyToVote.upvoteCount).to.equal(originalUpvote + 1);
+            expect(replyToVote.downvoteCount).to.equal(originalDownvote - 1);
+            expect(replyToVote.author.subplebbit.postScore).to.equal(1);
+            expect(replyToVote.author.subplebbit.replyScore).to.equal(1);
+            expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+        });
+
+        it("plebbit.createVote fails when commentCid is invalid ", async () => {
+            try {
+                await plebbit.createVote({
+                    vote: previousVotes[1].vote,
+                    subplebbitAddress: previousVotes[1].subplebbitAddress,
+                    signer: previousVotes[1].signer,
+                    commentCid: "gibbrish"
+                });
+                expect.fail("should fail");
+            } catch (e) {
+                expect(e.code).to.equal("ERR_INVALID_CREATE_VOTE_ARGS_SCHEMA");
+                expect(e.details.zodError.issues[0].message).to.equal("CID is invalid");
+            }
+        });
+
+        it(`Subplebbits rejects votes with invalid commentCid`);
+
+        // TODO add a test for spreading Vote instance
     });
-
-    it(`Subplebbits rejects votes with invalid commentCid`);
-
-    // TODO add a test for spreading Vote instance
 });
--- test/node-and-browser/publications/vote/upvote.test.js
+++ test/node-and-browser/publications/vote/upvote.test.js
@@ -5,129 +5,131 @@ import {
     publishRandomPost,
     publishRandomReply,
     publishWithExpectedResult,
-    mockRemotePlebbit,
-    resolveWhenConditionIsTrue
+    resolveWhenConditionIsTrue,
+    getRemotePlebbitConfigs
 } from "../../../../dist/node/test/test-util.js";
 import * as remeda from "remeda";
 
 const subplebbitAddress = signers[0].address;
 
-const previousVotes = [];
+getRemotePlebbitConfigs().map((config) => {
+    const previousVotes = [];
 
-describe("Test upvote", async () => {
-    let plebbit, postToVote, replyToVote, signer;
+    describe(`Test upvote - ${config.name}`, async () => {
+        let plebbit, postToVote, replyToVote, signer;
 
-    before(async () => {
-        plebbit = await mockRemotePlebbit();
-        signer = await plebbit.createSigner();
-        postToVote = await publishRandomPost(subplebbitAddress, plebbit, { signer }, false);
-        replyToVote = await publishRandomReply(postToVote, plebbit, { signer }, false);
-        await Promise.all([postToVote.update(), replyToVote.update()]);
-        await resolveWhenConditionIsTrue(postToVote, () => typeof postToVote.updatedAt === "number");
-        await resolveWhenConditionIsTrue(replyToVote, () => typeof replyToVote.updatedAt === "number");
-    });
-
-    after(async () => {
-        await postToVote.stop();
-        await replyToVote.stop();
-    });
-
-    it(`(vote: Vote) === plebbit.createVote(JSON.parse(JSON.stringify(vote)))`, async () => {
-        const vote = await generateMockVote(postToVote, 1, plebbit, remeda.sample(signers, 1)[0]);
-        const voteFromStringifiedVote = await plebbit.createVote(JSON.parse(JSON.stringify(vote)));
-        const jsonPropsToOmit = ["clients"];
-
-        const voteJson = remeda.omit(JSON.parse(JSON.stringify(vote)), jsonPropsToOmit);
-        const stringifiedVoteJson = remeda.omit(JSON.parse(JSON.stringify(voteFromStringifiedVote)), jsonPropsToOmit);
-        expect(voteJson.signer).to.be.a("object").and.to.deep.equal(stringifiedVoteJson.signer); // make sure internal props like signer are copied properly
-        expect(voteJson).to.deep.equal(stringifiedVoteJson);
-    });
-
-    it("Can upvote a post", async () => {
-        const originalUpvote = remeda.clone(postToVote.upvoteCount);
-        const vote = await generateMockVote(postToVote, 1, plebbit);
-        await publishWithExpectedResult(vote, true);
-        await resolveWhenConditionIsTrue(postToVote, () => postToVote.upvoteCount === originalUpvote + 1);
-        expect(postToVote.upvoteCount).to.be.equal(originalUpvote + 1);
-        expect(postToVote.downvoteCount).to.be.equal(0);
-        expect(postToVote.author.subplebbit.replyScore).to.equal(0);
-        expect(postToVote.author.subplebbit.postScore).to.equal(1);
-        expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-        previousVotes.push(vote);
-    });
-
-    it(`Can upvote a reply`, async () => {
-        const originalUpvote = remeda.clone(replyToVote.downvoteCount);
-        const vote = await generateMockVote(replyToVote, 1, plebbit);
-        await publishWithExpectedResult(vote, true);
-        await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.upvoteCount === originalUpvote + 1);
-        expect(replyToVote.upvoteCount).to.equal(originalUpvote + 1);
-        expect(replyToVote.downvoteCount).to.equal(0);
-        expect(replyToVote.author.subplebbit.replyScore).to.equal(1);
-        expect(replyToVote.author.subplebbit.postScore).to.equal(1);
-        expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-
-        previousVotes.push(vote);
-    });
-
-    it("Can change post upvote to downvote", async () => {
-        const originalUpvote = remeda.clone(postToVote.upvoteCount);
-        const originalDownvote = remeda.clone(postToVote.downvoteCount);
-        const vote = await plebbit.createVote({
-            commentCid: previousVotes[0].commentCid,
-            signer: previousVotes[0].signer,
-            subplebbitAddress: previousVotes[0].subplebbitAddress,
-            vote: -1
+        before(async () => {
+            plebbit = await config.plebbitInstancePromise();
+            signer = await plebbit.createSigner();
+            postToVote = await publishRandomPost(subplebbitAddress, plebbit, { signer }, false);
+            replyToVote = await publishRandomReply(postToVote, plebbit, { signer }, false);
+            await Promise.all([postToVote.update(), replyToVote.update()]);
+            await resolveWhenConditionIsTrue(postToVote, () => typeof postToVote.updatedAt === "number");
+            await resolveWhenConditionIsTrue(replyToVote, () => typeof replyToVote.updatedAt === "number");
         });
-        await publishWithExpectedResult(vote, true);
-        await resolveWhenConditionIsTrue(postToVote, () => postToVote.upvoteCount === originalUpvote - 1);
 
-        expect(postToVote.upvoteCount).to.equal(originalUpvote - 1);
-        expect(postToVote.downvoteCount).to.equal(originalDownvote + 1);
-        expect(postToVote.author.subplebbit.postScore).to.equal(-1);
-        expect(postToVote.author.subplebbit.replyScore).to.equal(1);
-        expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-    });
-
-    it("Can change reply upvote to downvote", async () => {
-        const originalUpvote = remeda.clone(replyToVote.upvoteCount);
-        const originalDownvote = remeda.clone(replyToVote.downvoteCount);
-        const vote = await plebbit.createVote({
-            commentCid: previousVotes[1].commentCid,
-            signer: previousVotes[1].signer,
-            subplebbitAddress: previousVotes[1].subplebbitAddress,
-            vote: -1
+        after(async () => {
+            await postToVote.stop();
+            await replyToVote.stop();
         });
-        await publishWithExpectedResult(vote, true);
-        await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.upvoteCount === originalUpvote - 1);
 
-        expect(replyToVote.upvoteCount).to.equal(originalUpvote - 1);
-        expect(replyToVote.downvoteCount).to.equal(originalDownvote + 1);
-        expect(replyToVote.author.subplebbit.postScore).to.equal(-1);
-        expect(replyToVote.author.subplebbit.replyScore).to.equal(-1);
-        expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
-    });
+        it(`(vote: Vote) === plebbit.createVote(JSON.parse(JSON.stringify(vote)))`, async () => {
+            const vote = await generateMockVote(postToVote, 1, plebbit, remeda.sample(signers, 1)[0]);
+            const voteFromStringifiedVote = await plebbit.createVote(JSON.parse(JSON.stringify(vote)));
+            const jsonPropsToOmit = ["clients"];
 
-    it("Does not throw an error when vote is duplicated", async () => {
-        const vote = await plebbit.createVote({
-            commentCid: previousVotes[0].commentCid,
-            signer: previousVotes[0].signer,
-            subplebbitAddress: previousVotes[0].subplebbitAddress,
-            vote: previousVotes[0].vote
+            const voteJson = remeda.omit(JSON.parse(JSON.stringify(vote)), jsonPropsToOmit);
+            const stringifiedVoteJson = remeda.omit(JSON.parse(JSON.stringify(voteFromStringifiedVote)), jsonPropsToOmit);
+            expect(voteJson.signer).to.be.a("object").and.to.deep.equal(stringifiedVoteJson.signer); // make sure internal props like signer are copied properly
+            expect(voteJson).to.deep.equal(stringifiedVoteJson);
         });
-        await publishWithExpectedResult(vote, true);
-    });
 
-    it(`Can publish a vote that was created from jsonfied vote instance`, async () => {
-        const vote = await generateMockVote(postToVote, 1, plebbit, remeda.sample(signers, 1)[0]);
-        const voteFromStringifiedVote = await plebbit.createVote(JSON.parse(JSON.stringify(vote)));
-        const challengeRequestPromise = new Promise((resolve) => voteFromStringifiedVote.once("challengerequest", resolve));
+        it("Can upvote a post", async () => {
+            const originalUpvote = remeda.clone(postToVote.upvoteCount);
+            const vote = await generateMockVote(postToVote, 1, plebbit);
+            await publishWithExpectedResult(vote, true);
+            await resolveWhenConditionIsTrue(postToVote, () => postToVote.upvoteCount === originalUpvote + 1);
+            expect(postToVote.upvoteCount).to.be.equal(originalUpvote + 1);
+            expect(postToVote.downvoteCount).to.be.equal(0);
+            expect(postToVote.author.subplebbit.replyScore).to.equal(0);
+            expect(postToVote.author.subplebbit.postScore).to.equal(1);
+            expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+            previousVotes.push(vote);
+        });
 
-        await publishWithExpectedResult(voteFromStringifiedVote, true);
-        const challengerequest = await challengeRequestPromise;
-        expect(challengerequest.vote).to.deep.equal(vote.toJSONPubsubMessagePublication());
+        it(`Can upvote a reply`, async () => {
+            const originalUpvote = remeda.clone(replyToVote.downvoteCount);
+            const vote = await generateMockVote(replyToVote, 1, plebbit);
+            await publishWithExpectedResult(vote, true);
+            await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.upvoteCount === originalUpvote + 1);
+            expect(replyToVote.upvoteCount).to.equal(originalUpvote + 1);
+            expect(replyToVote.downvoteCount).to.equal(0);
+            expect(replyToVote.author.subplebbit.replyScore).to.equal(1);
+            expect(replyToVote.author.subplebbit.postScore).to.equal(1);
+            expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
 
-        expect(vote.toJSONPubsubMessagePublication()).to.deep.equal(voteFromStringifiedVote.toJSONPubsubMessagePublication());
-        expect(vote.toJSONPubsubRequestToEncrypt()).to.deep.equal(voteFromStringifiedVote.toJSONPubsubRequestToEncrypt());
+            previousVotes.push(vote);
+        });
+
+        it("Can change post upvote to downvote", async () => {
+            const originalUpvote = remeda.clone(postToVote.upvoteCount);
+            const originalDownvote = remeda.clone(postToVote.downvoteCount);
+            const vote = await plebbit.createVote({
+                commentCid: previousVotes[0].commentCid,
+                signer: previousVotes[0].signer,
+                subplebbitAddress: previousVotes[0].subplebbitAddress,
+                vote: -1
+            });
+            await publishWithExpectedResult(vote, true);
+            await resolveWhenConditionIsTrue(postToVote, () => postToVote.upvoteCount === originalUpvote - 1);
+
+            expect(postToVote.upvoteCount).to.equal(originalUpvote - 1);
+            expect(postToVote.downvoteCount).to.equal(originalDownvote + 1);
+            expect(postToVote.author.subplebbit.postScore).to.equal(-1);
+            expect(postToVote.author.subplebbit.replyScore).to.equal(1);
+            expect(postToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+        });
+
+        it("Can change reply upvote to downvote", async () => {
+            const originalUpvote = remeda.clone(replyToVote.upvoteCount);
+            const originalDownvote = remeda.clone(replyToVote.downvoteCount);
+            const vote = await plebbit.createVote({
+                commentCid: previousVotes[1].commentCid,
+                signer: previousVotes[1].signer,
+                subplebbitAddress: previousVotes[1].subplebbitAddress,
+                vote: -1
+            });
+            await publishWithExpectedResult(vote, true);
+            await resolveWhenConditionIsTrue(replyToVote, () => replyToVote.upvoteCount === originalUpvote - 1);
+
+            expect(replyToVote.upvoteCount).to.equal(originalUpvote - 1);
+            expect(replyToVote.downvoteCount).to.equal(originalDownvote + 1);
+            expect(replyToVote.author.subplebbit.postScore).to.equal(-1);
+            expect(replyToVote.author.subplebbit.replyScore).to.equal(-1);
+            expect(replyToVote.author.subplebbit.lastCommentCid).to.equal(replyToVote.cid);
+        });
+
+        it("Does not throw an error when vote is duplicated", async () => {
+            const vote = await plebbit.createVote({
+                commentCid: previousVotes[0].commentCid,
+                signer: previousVotes[0].signer,
+                subplebbitAddress: previousVotes[0].subplebbitAddress,
+                vote: previousVotes[0].vote
+            });
+            await publishWithExpectedResult(vote, true);
+        });
+
+        it(`Can publish a vote that was created from jsonfied vote instance`, async () => {
+            const vote = await generateMockVote(postToVote, 1, plebbit, remeda.sample(signers, 1)[0]);
+            const voteFromStringifiedVote = await plebbit.createVote(JSON.parse(JSON.stringify(vote)));
+            const challengeRequestPromise = new Promise((resolve) => voteFromStringifiedVote.once("challengerequest", resolve));
+
+            await publishWithExpectedResult(voteFromStringifiedVote, true);
+            const challengerequest = await challengeRequestPromise;
+            expect(challengerequest.vote).to.deep.equal(vote.toJSONPubsubMessagePublication());
+
+            expect(vote.toJSONPubsubMessagePublication()).to.deep.equal(voteFromStringifiedVote.toJSONPubsubMessagePublication());
+            expect(vote.toJSONPubsubRequestToEncrypt()).to.deep.equal(voteFromStringifiedVote.toJSONPubsubRequestToEncrypt());
+        });
     });
 });
