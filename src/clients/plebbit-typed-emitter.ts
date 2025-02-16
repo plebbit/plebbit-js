43602c5df409b449f73ef9977614e9a656c8ea49
Rinse <rinse12@tutanota.com>
2025-02-16 10:22:15 +0000

    make sure to hide internal props of event emitter

--- src/clients/plebbit-typed-emitter.ts
+++ src/clients/plebbit-typed-emitter.ts
@@ -1,12 +1,18 @@
 import { ListenerSignature, TypedEmitter } from "tiny-typed-emitter";
+import { hideClassPrivateProps } from "../util";
 
 export class PlebbitTypedEmitter<T extends ListenerSignature<T>> extends TypedEmitter<T> {
-    mirroredClient?: PlebbitTypedEmitter<T>;
+    _mirroredClient?: PlebbitTypedEmitter<T> = undefined;
     state: any;
-    private stateListener?: (newState: string) => void;
+    private _stateListener?: (newState: string) => void = undefined;
+
+    constructor() {
+        super();
+        hideClassPrivateProps(this);
+    }
 
     mirror(sourceClient: PlebbitTypedEmitter<T>): void {
-        if (this.mirroredClient) {
+        if (this._mirroredClient) {
             throw new Error("This client is already mirroring another client");
         }
 
@@ -18,7 +24,7 @@ export class PlebbitTypedEmitter<T extends ListenerSignature<T>> extends TypedEm
         }
 
         // Setup state change listener
-        this.stateListener = (newState: string) => {
+        this._stateListener = (newState: string) => {
             this.state = newState;
             //@ts-expect-error
             this.emit("statechange", newState);
@@ -26,27 +32,27 @@ export class PlebbitTypedEmitter<T extends ListenerSignature<T>> extends TypedEm
 
         // Listen to state changes on source client
         //@ts-expect-error
-        sourceClient.on("statechange", this.stateListener);
+        sourceClient.on("statechange", this._stateListener);
 
         // Store reference to source client
-        this.mirroredClient = sourceClient;
+        this._mirroredClient = sourceClient;
     }
 
     unmirror() {
-        if (!this.mirroredClient || !this.stateListener) {
+        if (!this._mirroredClient || !this._stateListener) {
             throw new Error("This client is not mirroring any other client");
         }
 
         // Remove state change listener
         //@ts-expect-error
-        this.mirroredClient.off("statechange", this.stateListener);
+        this._mirroredClient.off("statechange", this._stateListener);
 
         // // Reset to stopped state
         // this.state = "stopped" as T;
         // this.emit("statechange", this.state);
 
         // Clear references
-        this.stateListener = undefined;
-        this.mirroredClient = undefined;
+        this._stateListener = undefined;
+        this._mirroredClient = undefined;
     }
 }
