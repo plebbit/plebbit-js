2b104c71fc8031835af15a1fd4e0e6acb9229cfc
Rinse <rinse12@tutanota.com>
2025-03-13 16:04:43 +0000

    raise db version to force new plebbit-js deps to use new db schema

--- src/version.ts
+++ src/version.ts
@@ -7,7 +7,7 @@ const userAgent = UserAgentSchema.parse(`/plebbit-js:${plebbitJsVersion}/`);
 
 export default {
     PLEBBIT_JS_VERSION: plebbitJsVersion,
-    DB_VERSION: 19,
+    DB_VERSION: 20,
     PROTOCOL_VERSION: protocolVersion,
     USER_AGENT: userAgent
 };
