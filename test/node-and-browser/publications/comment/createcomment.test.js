d41488b232fc746382fd2ac6fcb931aefe0d4625
Rinse <rinse12@tutanota.com>
2025-03-13 10:03:02 +0000

    wait until replyCount is there, should make test less likely to fail

--- test/node-and-browser/publications/comment/createcomment.test.js
+++ test/node-and-browser/publications/comment/createcomment.test.js
@@ -126,7 +126,7 @@ getRemotePlebbitConfigs().map((config) => {
             expect(post.replies).to.be.a("object");
             await publishRandomReply(post, plebbit);
             await post.update();
-            await resolveWhenConditionIsTrue(post, () => typeof post.updatedAt === "number");
+            await resolveWhenConditionIsTrue(post, () => post.replyCount === 1);
             expect(post.content).to.be.a("string");
             expect(post.replyCount).to.equal(1);
             expect(post.replies.pages.topAll.comments.length).to.equal(1);
