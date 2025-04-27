08ec395ab7908f71512b45b1624ada26ec87e8f7
Rinse <rinse12@tutanota.com>
2025-04-27 17:44:30 +0700

    clarify test

--- test/node-and-browser/publications/comment/pubsub.clients.test.js
+++ test/node-and-browser/publications/comment/pubsub.clients.test.js
@@ -4,9 +4,10 @@ import {
     generateMockPost,
     mockRemotePlebbit,
     publishWithExpectedResult,
-    mockGatewayPlebbit,
     generatePostToAnswerMathQuestion,
-    describeSkipIfRpc
+    describeSkipIfRpc,
+    mockPlebbitNoDataPathWithOnlyKuboClient,
+    mockPlebbitV2
 } from "../../../../dist/node/test/test-util.js";
 import { createMockPubsubClient } from "../../../../dist/node/test/mock-ipfs-client.js";
 
@@ -14,11 +15,14 @@ const subplebbitAddress = signers[0].address;
 const mathCliSubplebbitAddress = signers[1].address;
 
 describeSkipIfRpc(`comment.clients.pubsubKuboRpcClients`, async () => {
-    let plebbit, gatewayPlebbit;
-    before(async () => {
-        plebbit = await mockRemotePlebbit();
-        gatewayPlebbit = await mockGatewayPlebbit();
+    let plebbit;
+    beforeEach(async () => {
+        plebbit = await mockPlebbitNoDataPathWithOnlyKuboClient();
     });
+    afterEach(async () => {
+        await plebbit.destroy();
+    });
+
     it(`comment.clients.pubsubKuboRpcClients[url].state is stopped by default`, async () => {
         const mockPost = await generateMockPost(subplebbitAddress, plebbit);
         expect(Object.keys(mockPost.clients.pubsubKuboRpcClients).length).to.equal(3);
@@ -129,8 +133,12 @@ describeSkipIfRpc(`comment.clients.pubsubKuboRpcClients`, async () => {
     it(`Correct order of pubsubKuboRpcClients state when provider 1 is not responding and moving on to the other one`, async () => {
         const notRespondingPubsubUrl = "http://localhost:15005/api/v0"; // Should take msgs but not respond, never throws errors
         const upPubsubUrl = "http://localhost:15002/api/v0";
-        const plebbit = await mockRemotePlebbit({
-            pubsubKuboRpcClientsOptions: [notRespondingPubsubUrl, upPubsubUrl]
+        const plebbit = await mockPlebbitV2({
+            plebbitOptions: {
+                pubsubKuboRpcClientsOptions: [notRespondingPubsubUrl, upPubsubUrl]
+            },
+            forceMockPubsub: false,
+            remotePlebbit: true
         });
 
         plebbit.clients.pubsubKuboRpcClients[upPubsubUrl]._client = createMockPubsubClient(); // Use mock pubsub to be on the same pubsub as the sub
@@ -145,7 +153,7 @@ describeSkipIfRpc(`comment.clients.pubsubKuboRpcClients`, async () => {
 
         const actualStates = { [notRespondingPubsubUrl]: [], [upPubsubUrl]: [] };
 
-        for (const pubsubUrl of Object.keys(expectedStates))
+        for (const pubsubUrl of Object.keys(plebbit.clients.pubsubKuboRpcClients))
             mockPost.clients.pubsubKuboRpcClients[pubsubUrl].on("statechange", (newState) => actualStates[pubsubUrl].push(newState));
 
         await publishWithExpectedResult(mockPost, true);
