9cd86d15974d0418f4770d46f711b06f714beec3
Rinse <rinse12@tutanota.com>
2025-04-29 09:07:29 +0000

    fix test

--- test/node-and-browser/publications/comment/replies/replies.clients.test.js
+++ test/node-and-browser/publications/comment/replies/replies.clients.test.js
@@ -22,6 +22,12 @@ describe(`comment.replies.clients`, async () => {
         commentCid = subplebbit.posts.pages.hot.comments[0].cid;
         expect(commentCid).to.be.a("string");
     });
+
+    after(async () => {
+        await plebbit.destroy();
+        await gatewayPlebbit.destroy();
+    });
+
     describeSkipIfRpc(`comment.replies.clients.kuboRpcClients`, async () => {
         it(`comment.replies.clients.kuboRpcClients is {} for gateway plebbit`, async () => {
             const comment = await gatewayPlebbit.getComment(commentCid);
@@ -104,7 +110,7 @@ describe(`comment.replies.clients`, async () => {
             const comment = await multipleGatewayPlebbit.getComment(commentCid);
             await comment.update();
             await resolveWhenConditionIsTrue(comment, () => typeof comment.updatedAt === "number");
-            const mockedPageIpfs = await addStringToIpfs(JSON.stringify({ comments: [comment.replies.pages.best.comments[0].raw] }));
+            const mockedPageIpfs = await addStringToIpfs(JSON.stringify({ comments: [comment.raw] })); // wrong schema, but goal is to test for states
             comment.replies.pageCids.new = mockedPageIpfs; // random cid
 
             const expectedStates = {
