9febfc5c12d35fb3afcca8585cc28fe30df1c211
Rinse12 <Rinse12@users.noreply.github.com>
2025-05-07 08:24:43 +0000

    CI build

--- dist/browser/runtime/node/addresses-rewriter-proxy-server.js
+++ dist/browser/runtime/node/addresses-rewriter-proxy-server.js
@@ -75,7 +75,7 @@ export class AddressesRewriterProxyServer {
             const proxyReq = httpRequest(requestOptions);
             // Handle timeout
             proxyReq.setTimeout(60000, () => {
-                debug.error("Proxy request timed out", requestOptions, reqBody);
+                debug.trace("Proxy request timed out", requestOptions, reqBody);
                 proxyReq.destroy();
             });
             // Handle proxy request errors - make sure to close connections
