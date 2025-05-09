f8b64c85e0cb4a9d739754a48c65a14a08a9db98
Rinse <rinse12@tutanota.com>
2025-05-09 11:00:10 +0000

    add test for failing to ls pubsub topics

--- test/node/subplebbit/error.start.subplebbit.test.js
+++ test/node/subplebbit/error.start.subplebbit.test.js
@@ -5,7 +5,8 @@ import {
     resolveWhenConditionIsTrue,
     publishRandomPost,
     waitTillPostInSubplebbitInstancePages,
-    describeSkipIfRpc
+    describeSkipIfRpc,
+    mockPlebbitNoDataPathWithOnlyKuboClient
 } from "../../../dist/node/test/test-util.js";
 describeSkipIfRpc(`Local subplebbit emits errors properly in the publish loop`, async () => {
     let plebbit;
@@ -60,6 +61,36 @@ describeSkipIfRpc(`Local subplebbit emits errors properly in the publish loop`, 
 
         for (const error of errors) {
             expect(error.message).to.equal("Failed to copy a file");
+        }
+    });
+
+    it(`subplebbit.start can recover if pubsub.ls() fails`, async () => {
+        const sub = await createSubWithNoChallenge({}, plebbit);
+        await sub.start();
+        await resolveWhenConditionIsTrue(sub, () => typeof sub.updatedAt === "number");
+        const errors = [];
+        sub.on("error", (err) => errors.push(err));
+
+        const pubsubClient = sub._clientsManager.getDefaultPubsub()._client;
+
+        const originalPubsub = pubsubClient.pubsub.ls.bind(pubsubClient.pubsub);
+        pubsubClient.pubsub.ls = () => {
+            throw Error("Failed to ls pubsub topics");
+        };
+
+        await resolveWhenConditionIsTrue(sub, () => errors.length === 3, "error");
+
+        pubsubClient.pubsub.ls = originalPubsub;
+
+        const remotePlebbit = await mockPlebbitNoDataPathWithOnlyKuboClient();
+        await publishRandomPost(sub.address, remotePlebbit); // pubsub topic is working
+        await remotePlebbit.destroy();
+
+        await sub.delete();
+        expect(errors.length).to.be.greaterThan(0);
+
+        for (const error of errors) {
+            expect(error.message).to.equal("Failed to ls pubsub topics");
         }
     });
 });
