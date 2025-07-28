66155f687b3a93ead00afa2ffb1036ff247e0b89
Rinse <rinse12@tutanota.com>
2025-07-28 06:01:35 +0000

    fix ipns fields test

--- test/node-and-browser/subplebbit/ipns.subplebbit.test.js
+++ test/node-and-browser/subplebbit/ipns.subplebbit.test.js
@@ -1,33 +1,8 @@
-import { getAvailablePlebbitConfigsToTestAgainst } from "../../../dist/node/test/test-util.js";
+import { getAvailablePlebbitConfigsToTestAgainst, resolveWhenConditionIsTrue } from "../../../dist/node/test/test-util.js";
 import { expect } from "chai";
+import signers from "../../fixtures/signers.js";
 
-const fixture = {
-    address: "weaponized-autism.eth",
-    pubsubTopic: "12D3KooWJM1TaC9SF1oQq7KHwKUQE8nhCJfbePoQ8ipWiC9B5GUA",
-    signature: {
-        publicKey: "frcVczLMYgfibZ2gUU5TZVZWkKFwH6QGE+J2o197RcM",
-        signature: "nIgfHI/h9W+tx4ID2IMk9zCUXeWyzOK3zb77JG5EuOXvdDAlMsW4LiYZc/LNXf9WF0UIRkGUIbjxd9a1lbCTAA",
-        signedPropertyNames: [
-            "posts",
-            "challenges",
-            "encryption",
-            "address",
-            "createdAt",
-            "updatedAt",
-            "pubsubTopic",
-            "statsCid",
-            "protocolVersion",
-            "postUpdates",
-            "title",
-            "description",
-            "roles",
-            "lastPostCid",
-            "lastCommentCid",
-            "suggested"
-        ],
-        type: "ed25519"
-    }
-};
+const subplebbitAddress = signers[0].address;
 
 getAvailablePlebbitConfigsToTestAgainst().map((config) => {
     describe(`subplebbit.{ipnsName, ipnsPubsubTopic, ipnsPubsubTopicRoutingCid, pubsubTopicRoutingCid}`, async () => {
@@ -35,7 +10,14 @@ getAvailablePlebbitConfigsToTestAgainst().map((config) => {
         let subplebbit;
         before(async () => {
             plebbit = await config.plebbitInstancePromise();
-            subplebbit = await plebbit.createSubplebbit(fixture);
+            subplebbit = await plebbit.createSubplebbit({ address: subplebbitAddress });
+            expect(subplebbit.ipnsName).to.equal(undefined);
+            expect(subplebbit.ipnsPubsubTopic).to.equal(undefined);
+            expect(subplebbit.ipnsPubsubTopicRoutingCid).to.equal(undefined);
+            expect(subplebbit.pubsubTopicRoutingCid).to.equal(undefined);
+
+            await subplebbit.update();
+            await resolveWhenConditionIsTrue(subplebbit, () => subplebbit.updatedAt);
         });
 
         after(async () => {
@@ -43,19 +25,19 @@ getAvailablePlebbitConfigsToTestAgainst().map((config) => {
         });
 
         it("subplebbit.ipnsName should be a valid IPNS name", async () => {
-            expect(subplebbit.ipnsName).equal("12D3KooWJM1TaC9SF1oQq7KHwKUQE8nhCJfbePoQ8ipWiC9B5GUA");
+            expect(subplebbit.ipnsName).equal("12D3KooWN5rLmRJ8fWMwTtkDN7w2RgPPGRM4mtWTnfbjpi1Sh7zR");
         });
 
         it("subplebbit.ipnsPubsubTopic should be a valid pubsub topic", async () => {
-            expect(subplebbit.ipnsPubsubTopic).equal("/record/L2lwbnMvACQIARIgfrcVczLMYgfibZ2gUU5TZVZWkKFwH6QGE-J2o197RcM");
+            expect(subplebbit.ipnsPubsubTopic).equal("/record/L2lwbnMvACQIARIgtkPPciAVI7kfzmSHjazd0ekx8z9bCt9RlE5RnEpFRGo");
         });
 
         it("subplebbit.ipnsPubsubTopicRoutingCid should be a valid DHT key", async () => {
-            expect(subplebbit.ipnsPubsubTopicRoutingCid).equal("bafkreigf5ljbukix5wnbrms3rg2nnggrygpjso6qpmff4j2cj7zhjgwxjy");
+            expect(subplebbit.ipnsPubsubTopicRoutingCid).equal("bafkreiftvi7wgbdhbxnenslhu5sytlid73siolkd2syhdnjhnvn3mksggi");
         });
 
         it("subplebbit.pubsubTopicRoutingCid should be a valid CID", async () => {
-            expect(subplebbit.pubsubTopicRoutingCid).equal("bafkreigdyreuuffwcpnnnhwrz5pxdi2jey7iyv2i5ketmwc65rpe7zbzoa");
+            expect(subplebbit.pubsubTopicRoutingCid).equal("bafkreidwoelrflsx5dgll7s6jfkhsj6ffkfplde2j5dyino6t7m4ijutem");
         });
     });
 });
