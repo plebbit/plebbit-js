bed1139ef27effc77ae8d9c449b021a8298466d7
Rinse <rinse12@tutanota.com>
2025-09-16 09:47:26 +0000

    add more tests for loading a comment that was accepted

--- test/node/subplebbit/modqueue.subplebbit.test.js
+++ test/node/subplebbit/modqueue.subplebbit.test.js
@@ -16,12 +16,18 @@ import {
 // need to test when we no longer have pending comments, modQueue pageCids should be undefined
 
 describe(`Pending approval modqueue functionality`, async () => {
-    let plebbit, subplebbit, pendingComment;
+    let plebbit, subplebbit, pendingComment, modSigner;
 
     before(async () => {
         plebbit = await mockPlebbit();
         subplebbit = await plebbit.createSubplebbit();
         await subplebbit.start();
+        modSigner = await plebbit.createSigner();
+        await subplebbit.edit({
+            roles: {
+                [modSigner.address]: { role: "moderator" }
+            }
+        });
     });
 
     after(async () => {
@@ -63,7 +69,6 @@ describe(`Pending approval modqueue functionality`, async () => {
             expect(pendingComment.publishingState).to.equal("succeeded");
             expect(pendingComment.cid).to.be.a("string");
             const verification = await challengeVerificationPromise;
-            // TODO need to test only minimal props are there in verification.commentUpdate
             expect(pendingComment.pendingApproval).to.be.true;
             expect(verification.commentUpdate.pendingApproval).to.be.true;
             expect(Object.keys(verification.commentUpdate).sort()).to.deep.equal([
@@ -75,12 +80,17 @@ describe(`Pending approval modqueue functionality`, async () => {
             ]);
         });
 
-        it("Should exclude specific publication types from pending approval", async () => {
-            // TODO: Test that pending approval can exclude certain types
-            // TODO need to test for publications that should not support pending approval
-            // like vote, subplebbitEdit, commentModeration, commentEdit
-            // (e.g., only posts, not replies)
+        it("Should not include pendingApproval in commentIpfs", async () => {
+            // TODO: Test that pendingApproval field is not stored in IPFS content
+            expect(pendingComment.raw.comment.pendingApproval).to.not.exist;
         });
+
+        // TODO: Test that pending approval can exclude certain types
+        // TODO need to test for publications that should not support pending approval
+        // like vote, subplebbitEdit, commentModeration, commentEdit
+        // (e.g., only posts, not replies)
+
+        it("Should exclude specific publication types from pending approval");
     });
 
     describe("Pending approval storage", () => {
@@ -93,6 +103,11 @@ describe(`Pending approval modqueue functionality`, async () => {
             expect(pendingCommentInPage.cid).to.equal(pendingComment.cid);
             expect(pendingCommentInPage.updatedAt).to.be.undefined;
             expect(pendingCommentInPage.pendingApproval).to.be.true;
+        });
+
+        it(`pending post should not have postCid defined at its pages`, async () => {
+            const pageRaw = JSON.parse(await plebbit.fetchCid(subplebbit.modQueue.pageCids?.pendingApproval));
+            expect(pageRaw.comments[0].comment.postCid).to.be.undefined;
         });
 
         it(`pending comment should not appear in subplebbit.lastPostCid or subplebbit.lastCommentCid`, async () => {
@@ -114,55 +129,34 @@ describe(`Pending approval modqueue functionality`, async () => {
             const pendingCommentFromGatewayRequest = await fetch("http://localhost:18080/ipfs/" + pendingComment.cid);
             expect(pendingCommentFromGatewayRequest.status).to.equal(504); // should fail to load
         });
-        it(`pending comment should not appear in any pageCids or preloaded pages`);
-
-        it("Should limit pending approvals to maxPendingApprovalCount (default 500)", async () => {
-            // TODO: Test that pending approval queue respects size limit
-            // Should use subplebbit.settings.maxPendingApprovalCount
-        });
-
-        it("Should remove oldest pending comments when hitting maxPendingApprovalCount limit", async () => {
-            // TODO: Test that when maxPendingApprovalCount is reached,
-            // the oldest pending comments are removed to make room for new ones
-        });
-
-        it("Should mark pending comments with commentUpdate.pendingApproval: true", async () => {
-            // TODO: Test that pending comments have correct flag set
-        });
-
-        it("Should set comment.pendingApproval and commentUpdate.pendingApproval to true", async () => {
-            // TODO: Verify both in-memory comment.pendingApproval and
-            // corresponding commentUpdate.pendingApproval are true for pending items
+        it(`pending comment should not appear in any pageCids or preloaded pages`, async () => {
+            expect(Object.keys(subplebbit.posts.pages)).to.deep.equal([]);
+            expect(Object.keys(subplebbit.posts.pageCids)).to.deep.equal([]);
         });
     });
 
-    describe("Pending approval notifications", () => {
-        it("Should notify author via ChallengeVerificationMessage with pendingApproval: true", async () => {
-            // TODO: Test that author receives notification about pending approval status
-        });
-
-        it("Should not include pendingApproval in commentIpfs", async () => {
-            // TODO: Test that pendingApproval field is not stored in IPFS content
-        });
-
-        it("Should trigger a new update when a comment is added to pendingApproval", async () => {
-            // TODO: Verify that adding a new pending comment emits an update event
-            // and that modqueue pages reflect the new item
-        });
-    });
-
-    describe("Comment moderation approval process", () => {
+    describe("Comment moderation approval of pending comment", () => {
         it("Should approve comments using createCommentModeration with approved: true", async () => {
-            // TODO: Test approval process using comment moderation API
+            const commentModeration = await plebbit.createCommentModeration({
+                subplebbitAddress: subplebbit.address,
+                signer: modSigner,
+                commentModeration: { approved: true },
+                commentCid: pendingComment.cid
+            });
+
+            await publishWithExpectedResult(commentModeration, true);
+        });
+        it(`pending comment will receive updates now`, async () => {
+            await pendingComment.update();
+            await resolveWhenConditionIsTrue(pendingComment, () => pendingComment.updatedAt);
+            expect(pendingComment.updatedAt).to.be.a("number");
+            expect(pendingComment.pendingApproval).to.be.false;
         });
 
-        it("Should generate ChallengeVerificationMessage.commentUpdate.cid when approving", async () => {
-            // TODO: Test that approval generates proper verification message
-        });
-
-        it("Should not pin or publish comment updates for pending comments", async () => {
-            // TODO: Test resource optimization - no pinning/publishing for pending comments
-        });
+        it(`Approved comment now appears in pages`, async () => {});
+        it(`Approved comment now appears in lastPostCid and lastCommentCid`);
+        it(`Approved comment does not appear in modQueue.pageCids`);
+        it(`Approved comment shows up in subplebbit.postUpdates`);
 
         it("Should resolve conflicting moderations to removed when both approved and removed are set", async () => {
             // TODO: If one mod sets approved: true and another sets removed: true,
@@ -176,6 +170,22 @@ describe(`Pending approval modqueue functionality`, async () => {
         });
     });
 
+    describe(`Comment moderation rejection of pending comment`, async () => {
+        it(`Rejecting a pending comment will purge it from modQueue`);
+        it(`Rejecting a pending comment will remove it from database of subplebbit`);
+    });
+
+    describe(`Modqueue limits`, async () => {
+        it("Should limit pending approvals to maxPendingApprovalCount (default 500)", async () => {
+            // TODO: Test that pending approval queue respects size limit
+            // Should use subplebbit.settings.maxPendingApprovalCount
+        });
+
+        it("Should remove oldest pending comments when hitting maxPendingApprovalCount limit", async () => {
+            // TODO: Test that when maxPendingApprovalCount is reached,
+            // the oldest pending comments are removed to make room for new ones
+        });
+    });
     describe("Modqueue page validation", () => {
         it("Should fail if a modqueue comment belongs to a different sub", async () => {
             // TODO: Ensure cross-sub comments cannot appear under another sub's modqueue
