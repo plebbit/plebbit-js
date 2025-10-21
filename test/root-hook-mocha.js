261c40f2672a2b6f69d79530aac2420769e6b302
Rinse <rinse12@tutanota.com>
2025-10-21 13:51:23 +0000

    remove infinite depth with DEBUG cause it clutters CI logs

--- test/root-hook-mocha.js
+++ test/root-hook-mocha.js
@@ -1,12 +1,12 @@
 import { inspect } from "node:util";
 
 // Expand debug logging to print deeply nested objects while running tests.
-if (!process.env.DEBUG_DEPTH) {
-    process.env.DEBUG_DEPTH = "null";
-}
+// if (!process.env.DEBUG_DEPTH) {
+//     process.env.DEBUG_DEPTH = "null";
+// }
 
-inspect.defaultOptions.depth = null;
-inspect.defaultOptions.colors = false;
+// inspect.defaultOptions.depth = null;
+// inspect.defaultOptions.colors = false;
 
 export const mochaHooks = {
     afterAll: () => {}
