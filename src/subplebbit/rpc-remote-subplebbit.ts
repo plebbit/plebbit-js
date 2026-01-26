dfa4a1054fa2ae498a7064b8b09d079292f2adf1
Rinse <rinse12@tutanota.com>
2026-01-26 04:39:20 +0000

    fix: ensure updatingState is set before emitting update event in RPC
    Previously, _processUpdateEventFromRpcUpdate conditionally set updatingState
    only if updateRecord.updatingState was truthy. In race conditions during
    parallel test execution with shared RPC server, this field could be undefined,
    causing updatingState to not be "succeeded" when the "update" event fired.
    
    Now always set the state, defaulting to "succeeded" if undefined.

--- src/subplebbit/rpc-remote-subplebbit.ts
+++ src/subplebbit/rpc-remote-subplebbit.ts
@@ -76,7 +76,7 @@ export class RpcRemoteSubplebbit extends RemoteSubplebbit {
         this.initSubplebbitIpfsPropsNoMerge(updateRecord.subplebbit);
         this.updateCid = updateRecord.updateCid;
 
-        if (updateRecord.updatingState) this._setUpdatingStateNoEmission(updateRecord.updatingState);
+        this._setUpdatingStateNoEmission(updateRecord.updatingState || "succeeded");
 
         this.emit("update", this);
     }
